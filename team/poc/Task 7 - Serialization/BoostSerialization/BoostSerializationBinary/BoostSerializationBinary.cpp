69308bf1b5a633c5906d4696ae42365ea58c29ca
Lince3D <Lince3D@7af85c2e-922d-2424-809b-042d02d0a1c2>
2010-10-30 12:24:21 +0000

    Prueba de charset.

--- team/poc/Task 7 - Serialization/BoostSerialization/BoostSerializationBinary/BoostSerializationBinary.cpp
+++ team/poc/Task 7 - Serialization/BoostSerialization/BoostSerializationBinary/BoostSerializationBinary.cpp
@@ -15,7 +15,9 @@
 #include <boost/serialization/map.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/array.hpp>
-
+ó
+    ñ
+    Á
 // Additional Boost include, only to illustrate its usage
 #include <boost/foreach.hpp>
 
