ff35cab0fa096dcc8146dc91997de0f7607f057b
andersonjag.blg <andersonjag.blg@gmail.com>
2011-03-18 08:27:12 +0000

    109-QMatrix3x3 Extensión 0001 Añadida la funcionalidad descrita en la Extensión 0001

--- source/QMatrix3x3.cpp
+++ source/QMatrix3x3.cpp
@@ -2,6 +2,7 @@
 
 #include "QAngle.h"
 #include "QMatrix3x3.h"
+#include "QMatrix3x4.h"
 
 namespace Kinesis
 {
@@ -68,6 +69,7 @@ namespace Math
 
     QMatrix3x3 operator*(const float_q &fScalar, const QBaseMatrix3x3 &m) 
     {
+
         QMatrix3x3 aux;
 
         aux.ij[0][0] = fScalar * m.ij[0][0];
@@ -100,6 +102,44 @@ namespace Math
         aux.ij[2][2] = this->ij[2][0]*m.ij[0][2] + this->ij[2][1]*m.ij[1][2] + this->ij[2][2]*m.ij[2][2];
 
         return aux;
+    }
+
+	QBaseMatrix3x4 QMatrix3x3::operator*(const QBaseMatrix3x4& m) const
+	{
+        QMatrix3x4 aux;
+
+        aux.ij[0][0] = this->ij[0][0]*m.ij[0][0] + this->ij[0][1]*m.ij[1][0] + this->ij[0][2]*m.ij[2][0];
+        aux.ij[0][1] = this->ij[0][0]*m.ij[0][1] + this->ij[0][1]*m.ij[1][1] + this->ij[0][2]*m.ij[2][1];
+        aux.ij[0][2] = this->ij[0][0]*m.ij[0][2] + this->ij[0][1]*m.ij[1][2] + this->ij[0][2]*m.ij[2][2];
+		aux.ij[0][3] = this->ij[0][0]*m.ij[0][3] + this->ij[0][1]*m.ij[1][3] + this->ij[0][2]*m.ij[2][3];
+
+        aux.ij[1][0] = this->ij[1][0]*m.ij[0][0] + this->ij[1][1]*m.ij[1][0] + this->ij[1][2]*m.ij[2][0];
+        aux.ij[1][1] = this->ij[1][0]*m.ij[0][1] + this->ij[1][1]*m.ij[1][1] + this->ij[1][2]*m.ij[2][1];
+        aux.ij[1][2] = this->ij[1][0]*m.ij[0][2] + this->ij[1][1]*m.ij[1][2] + this->ij[1][2]*m.ij[2][2];
+		aux.ij[1][3] = this->ij[1][0]*m.ij[0][3] + this->ij[1][1]*m.ij[1][3] + this->ij[1][2]*m.ij[2][3];
+
+        aux.ij[2][0] = this->ij[2][0]*m.ij[0][0] + this->ij[2][1]*m.ij[1][0] + this->ij[2][2]*m.ij[2][0];
+        aux.ij[2][1] = this->ij[2][0]*m.ij[0][1] + this->ij[2][1]*m.ij[1][1] + this->ij[2][2]*m.ij[2][1];
+        aux.ij[2][2] = this->ij[2][0]*m.ij[0][2] + this->ij[2][1]*m.ij[1][2] + this->ij[2][2]*m.ij[2][2];
+        aux.ij[2][3] = this->ij[2][0]*m.ij[0][3] + this->ij[2][1]*m.ij[1][3] + this->ij[2][2]*m.ij[2][3];
+
+        return aux;
+
+	}
+
+    QMatrix3x3& QMatrix3x3::operator*=(const float_q& fScalar)
+    {
+        this->ij[0][0] *= fScalar;
+		this->ij[0][1] *= fScalar;
+		this->ij[0][2] *= fScalar;
+		this->ij[1][0] *= fScalar;
+		this->ij[1][1] *= fScalar;
+		this->ij[1][2] *= fScalar;
+		this->ij[2][0] *= fScalar;
+		this->ij[2][1] *= fScalar;
+		this->ij[2][2] *= fScalar;
+
+        return *this;
     }
 
     QMatrix3x3 QMatrix3x3::operator/(const float_q &fScalar) const
