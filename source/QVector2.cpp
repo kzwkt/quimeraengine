e8af42abdf3ddb7bcf2bf1e3688f478d270b5880
marcial.rial <marcial.rial@gmail.com>
2011-04-10 21:48:58 +0000

    116 - QVector2 Extension 002. Añade funcionalidad relacionada con transformaciones espaciales aplicables a vectores de 2 dimensiones.
    Se modifican los archivos QVector2.h y QVector2.cpp

--- source/QVector2.cpp
+++ source/QVector2.cpp
@@ -1,6 +1,8 @@
 // [TERMS&CONDITIONS]
 
 #include "QVector2.h"
+#include "QAngle.h"
+#include "QTransformationMatrix3x3.h"
 
 namespace Kinesis
 {
@@ -99,6 +101,54 @@ string_q QVector2::ToString()
 	return QE_L("V2(") + QFloat::ToString(this->x) + 
 		   QE_L(", ")  + QFloat::ToString(this->y) + 
 		   QE_L(")");
+}
+
+void QVector2::Transform(const QTransformationMatrix3x3& matrix)
+{
+	float_q fNewX = this->x * matrix.ij[0][0] + this->y * matrix.ij[1][0] + matrix.ij[2][0];
+	float_q fNewY = this->x * matrix.ij[0][1] + this->y * matrix.ij[1][1] + matrix.ij[2][1];
+
+	this->x = fNewX;
+	this->y = fNewY;
+}
+
+void QVector2::Transform(const QTransformationMatrix3x3& matrix, QBaseVector2& vectorOut)
+{
+	vectorOut.x = this->x * matrix.ij[0][0] + this->y * matrix.ij[1][0] + matrix.ij[2][0];
+	vectorOut.y = this->x * matrix.ij[0][1] + this->y * matrix.ij[1][1] + matrix.ij[2][1];
+}
+
+void QVector2::Transform(const float_q& fAngle)
+{
+	#if QE_CONFIG_ANGLENOTATION_DEFAULT == QE_CONFIG_ANGLENOTATION_DEGREES
+		float_q fAngleRad = QAngle::DegreesToRadians(fAngle);
+	#else
+		float_q fAngleRad = fAngle;
+	#endif
+
+	const float_q fCosAngle = cos(fAngleRad);
+	const float_q fSinAngle = sin(fAngleRad);
+
+	float_q fNewX = this->x * fCosAngle - this->y * fSinAngle;
+	float_q fNewY = this->y * fCosAngle + this->x * fSinAngle;
+
+	this->x = fNewX;
+	this->y = fNewY;
+}
+
+void QVector2::Transform(const float_q& fAngle, QBaseVector2& vectorOut)
+{
+	#if QE_CONFIG_ANGLENOTATION_DEFAULT == QE_CONFIG_ANGLENOTATION_DEGREES
+		float_q fAngleRad = QAngle::DegreesToRadians(fAngle);
+	#else
+		float_q fAngleRad = fAngle;
+	#endif
+
+	const float_q fCosAngle = cos(fAngleRad);
+	const float_q fSinAngle = sin(fAngleRad);
+
+	vectorOut.x = this->x * fCosAngle - this->y * fSinAngle;
+	vectorOut.y = this->y * fCosAngle + this->x * fSinAngle;
 }
 
 } //namespace Math
