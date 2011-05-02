15738e637345d39de69237dc321eac2d36bc6a63
jwladi@gmail.com <jwladi@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-05-02 04:33:08 +0000

    132 - QPlane Extension 001 Adds functionalities to QPlane class. Adds enumerated class EQIntersections.

--- trunk/headers/QPlane.h
+++ trunk/headers/QPlane.h
@@ -3,6 +3,8 @@
 #ifndef __QPLANE__
 #define __QPLANE__
 
+#include "EQIntersections.h"
+#include "EQSpaceRelation.h"
 #include "QBasePlane.h"
 
 namespace Kinesis
@@ -99,6 +101,27 @@ public:
 	/// The parse order: 1st value (a coefficient), 2nd value (b coefficient), 
 	/// 3rd value (c coefficient), 4th value (d independent term).</param>
     inline explicit QPlane(const vf32_q vfValues) : QBasePlane(vfValues) { }
+
+	/// <summary>
+	/// Constructor from three points.
+	/// </summary>
+	/// <param name="vP1">[IN] A 3D vector which represents the first point.</param>
+	/// <param name="vP2">[IN] A 3D vector which represents the second point.</param>
+	/// <param name="vP3">[IN] A 3D vector which represents the third point.</param>
+    template <class VectorType>
+    inline QPlane (const VectorType &vP1, const VectorType &vP2, const VectorType &vP3)
+    {
+        // Creates two vectors, to obtain the direction vector of the plane via cross product
+        VectorType vAux1(vP2 - vP1);
+        
+        vAux1.CrossProduct(vP3 - vP1);
+
+        // Checkout to avoid the possibility of tree colineal points.
+        QE_ASSERT (vAux1.IsNotZero());
+
+        // Plane equation
+        *this = QBasePlane(vAux1.x, vAux1.y, vAux1.z, -(vAux1.DotProduct(vP1)));
+    }
 
     // METHODS
 	// ---------------
@@ -266,11 +289,276 @@ public:
     /// Calculates a normalized plane from the resident one, storing it in an output plane provided. 
     /// This implies to divide all components of plane by the length of the normal.
     /// </summary>
-    /// <param name="pOut">[OUT] The plane where we want to store the normalized plane.</param>
-    inline void Normalize(QBasePlane &pOut)
+    /// <param name="plOut">[OUT] The plane where we want to store the normalized plane.</param>
+    inline void Normalize(QBasePlane &plOut) const
     {
-        pOut = *this;
-        reinterpret_cast <QPlane&> (pOut).Normalize();
+        plOut = *this;
+        reinterpret_cast <QPlane&> (plOut).Normalize();
+    }
+
+    /// <summary>
+    /// Calculates the dot product between the direction vector of the resident plane and the provided vector.
+    /// </summary>
+    /// <param name="v">[IN] The vector which we want to calculate the dot product with.</param>
+    template <class VectorType>
+    inline float_q DotProduct(const VectorType &v) const
+    {
+        return v.x * this->a + v.y * this->b + v.z * this->c; 
+    }
+
+    /// <summary>
+    /// Calculates the dot product between the direction vectors of the resident plane and the provided one.
+    /// </summary>
+    /// <param name="p">[IN] The plane which we want to calculate the dot product with.</param>
+    template <>
+    inline float_q DotProduct<QBasePlane>(const QBasePlane &p) const
+    {
+        return p.a * this->a + p.b * this->b + p.c * this->c; 
+    }
+
+    /// <summary>
+    /// Calculates the angle between the direction vector of the resident plane and the provided vector via dot product.
+    /// </summary>
+    /// <param name="v">[IN] The plane whose angle with the resident plane we want to calculate.</param>
+    template <class VectorType>
+    inline float_q DotProductAngle(const VectorType &v) const
+    {
+        const float_q &fDotLength = sqrt(this->GetSquaredLength() * v.GetSquaredLength());
+      
+        // Checkout to avoid division by zero.        
+        QE_ASSERT(fDotLength != QFloat::0);
+
+        const float_q &fDot = this->DotProduct(v)/fDotLength;
+
+        // Checkout to avoid undefined values of acos. Remember that -1 <= cos(angle) <= 1.
+        QE_ASSERT(abs(fDot) <= QFloat::_1);
+
+        const float_q &fAngle = acos(fDot);
+        #if QE_CONFIG_ANGLENOTATION_DEFAULT == QE_CONFIG_ANGLENOTATION_DEGREES
+            // If angles are specified in degrees, then converts angle to degrees
+            fAngle = QAngle::RadiansToDegrees(fAngle, fAngle);
+        #endif
+
+        return fAngle;
+    }
+
+    /// <summary>
+    /// Calculates the angle between resident and provided planes via dot product between their direction vectors.
+    /// </summary>
+    /// <param name="p">[IN] The plane whose angle with the resident plane we want to calculate.</param>
+    template <>
+    inline float_q DotProductAngle<QBasePlane>(const QBasePlane &p) const
+    {
+        const float_q fDotLength = sqrt(this->GetSquaredLength() * reinterpret_cast<const QPlane&>(p).GetSquaredLength());
+
+        // Checkout to avoid division by zero.        
+        QE_ASSERT(fDotLength != QFloat::_0);
+
+        const float_q &fDot = this->DotProduct(p)/fDotLength;
+
+        // Checkout to avoid undefined values of acos. Remember that -1 <= cos(angle) <= 1.
+        QE_ASSERT(abs(fDot) <= QFloat::_1);
+
+        const float_q &fAngle = acos(fDot);
+        #if QE_CONFIG_ANGLENOTATION_DEFAULT == QE_CONFIG_ANGLENOTATION_DEGREES
+            // If angles are specified in degrees, then converts angle to degrees
+            fAngle = QAngle::RadiansToDegrees(fAngle, fAngle);
+        #endif
+
+        return fAngle;
+    }
+
+    /// <summary>
+    /// Calculates the orthogonal projection of a given point over the resident plane.
+    /// </summary>
+    /// <param name="vPoint">[IN] A 3D vector which represents the point we want project.</param>
+    /// <param name="vProjection">[OUT] A 3D vector where to store the projected point.</param>
+    template <class VectorType>
+    inline void PointProjection(const VectorType &vPoint, VectorType &vProjection) const
+    {
+        const float_q &fSquaredLength = this->GetSquaredLength();
+
+        QE_ASSERT(fSquaredLength != QFloat::0);
+
+        const float_q &fProj = -(this->a * vPoint.x + this->b * vPoint.y + this->c * vPoint.z + this->d) / fSquaredLength;
+
+        vProjection.x = fProj * this->a  + vPoint.x;
+        vProjection.y = fProj * this->b  + vPoint.y;
+        vProjection.z = fProj * this->c  + vPoint.z;
+    }
+
+    /// <summary>
+    /// Calculates if a point is contained on the resident plane.
+    /// </summary>
+    /// <param name="v">[IN] The point we want know if it is contained on the resident plane.</param>
+    /// <returns>
+    /// True if the point is contained, false otherwise.
+    /// </returns>
+    template <class VectorType>
+    inline bool Contains(const VectorType &v) const
+    {
+        return QFloat::IsZero(this->a * v.x + this->b * v.y + this->c * v.z + this->d);
+    }
+
+    /// <summary>
+    /// Calculates the length of the direction vector of the resident plane.
+    /// </summary>
+    /// <returns>
+    /// A floating point value representing the value of the calculated length.
+    /// </returns>
+    inline float_q GetLength() const
+    {
+        return sqrt(this->a * this->a + this->b * this->b + this->c * this->c);
+    }
+
+    /// <summary>
+    /// Calculates the squared length of the direction vector of the resident plane.
+    /// </summary>
+    /// <returns>
+    /// A floating point value representing the value of the calculated squared length.
+    /// </returns>
+    inline float_q GetSquaredLength() const
+    {
+        return this->a * this->a + this->b * this->b + this->c * this->c;
+    }
+
+    /// <summary>
+    /// Calculates the direction vector of the resident plane.
+    /// </summary>
+    /// <param name="vOut">[OUT] Vector to store the direction vector.</param>
+    template <class VectorType>
+    inline void GetNormal(VectorType &vOut) const
+    {
+        vOut.x = this->a;
+        vOut.y = this->b;
+        vOut.z = this->c;
+    }
+
+    /// <summary>
+    /// Calculates the minimum distance from the given point to the resident plane, which is the length
+    /// of the normal to the plane which passes through the point.
+    /// </summary>
+    /// <param name="v">[IN] The point we want know it distance from resident plane.</param>
+    /// <returns>
+    /// A floating point value which represents the minimum distance between the plane and the point.
+    /// </returns>
+    template <class VectorType>
+    inline float_q PointDistance(const VectorType &v) const
+    {
+        const float_q &fLength = this->GetLength();
+
+        QE_ASSERT(fLength != QFloat::0);
+
+        return abs(this->a * v.x + this->b * v.y + this->c * v.z + this->d)/fLength;
+    }
+
+
+    /// <summary>
+    /// Calculates the number of intersections between the resident plane and two planes provided, 
+    /// and also calculates the intersection point if there is only one.
+    /// </summary>
+    /// <param name="pl1">[IN] The first plane we want to calculate the intersection with.</param>
+    /// <param name="pl2">[IN] The second plane we want to calculate the intersection with.</param>
+    /// <param name="vOut">[OUT] The intersection point of the three planes, if it exists.</param>
+    /// <returns>
+    /// An enumerated value which represents the number of intersections between the three planes, and can take 
+    /// the following values: E_None, E_One and E_Infinite.
+    /// </returns>
+    template <class VectorType>
+    EQIntersections IntersectionPoint (const QBasePlane &pl1, const QBasePlane &pl2, VectorType &vOut) const
+    {
+        // Solved by Cramer method.
+        const float_q &fDetC = this->a*pl1.b*pl2.c + this->b*pl1.c*pl2.a + this->c*pl1.a*pl2.b - 
+                              (this->c*pl1.b*pl2.a + this->a*pl1.c*pl2.b + this->b*pl1.a*pl2.c);
+
+        const float_q &fDetX = this->c*pl1.b*pl2.d + this->d*pl1.c*pl2.b + this->b*pl1.d*pl2.c - 
+                              (this->d*pl1.b*pl2.c + this->b*pl1.c*pl2.d + this->c*pl1.d*pl2.b);
+        const float_q &fDetY = this->c*pl1.d*pl2.a + this->a*pl1.c*pl2.d + this->d*pl1.a*pl2.c - 
+                              (this->a*pl1.d*pl2.c + this->d*pl1.c*pl2.a + this->c*pl1.a*pl2.d);
+        const float_q &fDetZ = this->d*pl1.b*pl2.a + this->a*pl1.d*pl2.b + this->b*pl1.a*pl2.d - 
+                              (this->a*pl1.b*pl2.d + this->b*pl1.d*pl2.a + this->d*pl1.a*pl2.b);
+
+        // A range = 3, A* range = 3: Compatible system
+        if (!QFloat::IsZero(fDetC))
+        {
+            const float_q &fInvDetC = QFloat::_1/fDetC;
+
+            vOut.x = fDetX * fInvDetC;
+            vOut.y = fDetY * fInvDetC;
+            vOut.z = fDetZ * fInvDetC;
+
+            return EQIntersections::E_One;
+        } 
+        // A range < 3, A* range < 3
+        else if (QFloat::IsZero(fDetX) && QFloat::IsZero(QfDetY) && QFloat::IsZero(fDetZ))
+        {
+            // A range = 2, A* range < 3: Undetermined compatible system
+            if (QFloat::IsNotZero(this->a*pl1.b - this->b*pl1.a) ||
+                QFloat::IsNotZero(this->a*pl1.c - this->c*pl1.a) ||
+                QFloat::IsNotZero(this->a*pl2.b - this->b*pl2.a) ||
+                QFloat::IsNotZero(this->a*pl2.c - this->c*pl2.a) ||
+                QFloat::IsNotZero(this->b*pl1.c - this->c*pl1.b) ||
+                QFloat::IsNotZero(this->b*pl2.c - this->c*pl2.b) ||
+                QFloat::IsNotZero(pl1.a*pl2.b - pl1.b*pl2.a) ||
+                QFloat::IsNotZero(pl1.a*pl2.c - pl1.c*pl2.a) ||
+                QFloat::IsNotZero(pl1.b*pl2.c - pl1.c*pl2.b))
+
+                return EQIntersections::E_Infinite;
+
+            //// A range = 1, A* range < 3
+            else
+            {
+                QPlane plAux1(*this), plAux2(pl1), plAux3(pl2);
+                plAux1.Normalize();
+                plAux2.Normalize();
+                plAux3.Normalize();
+
+                // Coincident planes (A* range = 1)
+                if (plAux1 == plAux2 && plAux1 == plAux3)
+
+                    return EQIntersections::E_Infinite;
+
+                // Incompatible system (A* range = 2)
+                else
+                    return EQIntersections::E_None;
+            }
+        }
+        // A range < 3, A* range = 3: Incompatible system
+        else
+           return EQIntersections::E_None;
+    }
+
+    /// <summary>
+    /// Checks the relation between resident plane and the provided plane.
+    /// </summary>
+    /// <param name="p">[IN] The plane we want check the relation with resident plane.</param>
+    /// <returns>
+    /// An enumerated value like follows: E_Contained, E_PositiveSide, E_NegativeSide.
+    /// </returns>
+    EQSpaceRelation SpaceRelation(const QBasePlane &p) const
+    {
+        // Cross product.
+        if (QFloat::IsZero(p.b*this->c - p.c*this->b) && 
+            QFloat::IsZero(p.c*this->a - p.a*this->c) && 
+            QFloat::IsZero(p.a*this->b - p.b*this->a))
+        {
+
+            QPlane plAux1(p), plAux2(*this);
+
+            plAux1.Normalize();
+            plAux2.Normalize();
+
+            // They are the same plane.
+            if (plAux1 == plAux2)
+                return EQSpaceRelation::E_Contained;
+            // They are parallel planes.
+            else if (QFloat::IsNegative(plAux1.d - plAux2.d))
+                return EQSpaceRelation::E_NegativeSide;
+            else
+                return EQSpaceRelation::E_PositiveSide;
+        }
+        else
+            return EQSpaceRelation::E_BothSides; 
     }
 
     /// <summary>
@@ -283,7 +571,9 @@ public:
         return QE_L("PL(") + QFloat::ToString(this->a) + QE_L(", ") + QFloat::ToString(this->b) + 
                QE_L(", ")  + QFloat::ToString(this->c) + QE_L(", ") + QFloat::ToString(this->d) + QE_L(")");
     }
+
 };
+
 
 } //namespace Math
 } //namespace Tools
