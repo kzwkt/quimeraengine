8de7be81f9cb0cc0d783a14b8abfdcf57c249fd3
jwladi <jwladi@gmail.com>
2011-05-02 04:33:08 +0000

    132 - QPlane Extension 001 Adds functionalities to QPlane class. Adds enumerated class EQIntersections.

---
+++ headers/EQIntersections.h
@@ -0,0 +1,274 @@
+// [TERMS&CONDITIONS]
+
+#ifndef __EQINTERSECTIONS__
+#define __EQINTERSECTIONS__
+
+#include <map>
+
+#include "DataTypesDefinitions.h"
+
+using Kinesis::QuimeraEngine::Tools::DataTypes::string_q;
+
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace Tools
+{
+namespace Math
+{
+
+/// <summary>
+/// Enumerates the number of intersections between objects or entities.
+/// </summary>
+class EQIntersections
+{
+    // ENUMERATIONS
+    // ---------------
+public:
+
+    /// <summary>
+    /// The encapsulated enumeration.
+    /// </summary>
+    enum EnumType
+    {
+        E_None = QE_ENUMERATION_MIN_VALUE, /*!< There are no intersections. */
+        E_One, /*!< There is one intersection. */
+        E_Two, /*!< There are two intersections. */
+        E_More, /*!< There are a finite number of intersections, greater than two. */
+        // [TODO] jwladi: It could be interesting to divide this in two values: E_MoreEven and E_MoreOdd.
+        E_Infinite, /*!< There are infinite intersections (the intersection may be a straight line or a plane. */
+        
+        _NotEnumValue = QE_ENUMERATION_MAX_VALUE /*!< Not valid value. */
+    };
+
+    // TYPEDEFS
+    // ---------------
+public:
+
+    typedef std::map<string_q, EQIntersections::EnumType> TNameValueMap;
+    typedef std::pair<string_q, EQIntersections::EnumType> TNameValuePair;
+
+
+	// METHODS
+	// ---------------
+public:
+
+    /// <summary>
+    /// Constructor that receives a valid enumeration value.
+    /// </summary>
+    /// <param name="eValue">A valid enumeration value.</param>
+    inline EQIntersections(const EQIntersections::EnumType &eValue) : m_value(eValue) 
+    {}
+
+    /// <summary>
+    /// Constructor that receives an integer number which must correspond to a valid enumeration value.
+    /// </summary>
+    /// <param name="nValue">An integer number.</param>
+    template<typename IntegerType>
+    inline EQIntersections(const IntegerType &nValue) : m_value(static_cast<const EQIntersections::EnumType>(nValue))
+    {}
+
+    /// <summary>
+    /// Constructor that receives the name of a valid enumeration value. Note that enumeration value names don't include
+    /// the enumeration prefix.
+    /// </summary>
+    /// <param name="strValueName">The name of a valid enumeration value.</param>
+    inline EQIntersections(const string_q &strValueName)
+    {
+        *this = strValueName;
+    }
+
+    /// <summary>
+    /// Assign operator that accepts an integer number that corresponds to a valid enumeration value.
+    /// </summary>
+    /// <param name="nValue">An integer number.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    template<typename IntegerType>
+    inline EQIntersections& operator=(const IntegerType &nValue)
+    {
+        m_value = static_cast<const EQIntersections::EnumType>(nValue);
+        return *this;
+    }
+
+    /// <summary>
+    /// Assign operator that accepts a valid enumeration value name.
+    /// </summary>
+    /// <param name="strValueName">The enumeration value name.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    inline EQIntersections& operator=(const string_q &strValueName)
+    {
+        if(EQIntersections::sm_mapValueName.find(strValueName) != EQIntersections::sm_mapValueName.end())
+            m_value = sm_mapValueName[strValueName];
+        else
+            m_value = EQIntersections::_NotEnumValue;
+
+        return *this;
+    }
+
+    /// <summary>
+    /// Assign operator that accepts a valid enumeration value.
+    /// </summary>
+    /// <param name="nValue">A valid enumeration value.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    inline EQIntersections& operator=(const EQIntersections::EnumType &eValue)
+    {
+        m_value = eValue;
+        return *this;
+    }
+
+    /// <summary>
+    /// Equality operator that accepts the name of a valid enumeration value. Note that enumeration value names don't include
+    /// the enumeration prefix.
+    /// </summary>
+    /// <param name="strValueName">The enumeration value name.</param>
+    /// <returns>
+    /// True if the name corresponds to a valid enumeration value and it equals the contained value. False otherwise.
+    /// </returns>
+    inline bool operator==(const string_q &strValueName) const
+    {
+        if(EQIntersections::sm_mapValueName.find(strValueName) != EQIntersections::sm_mapValueName.end())
+            return m_value == sm_mapValueName[strValueName];
+        else
+            return false;
+    }
+
+
+    /// <summary>
+    /// Equality operator that accepts an integer number which must correspond to a valid enumeration value.
+    /// </summary>
+    /// <param name="strValueName">An integer number.</param>
+    /// <returns>
+    /// True if the number corresponds to a valid enumeration value and it equals the contained value. False otherwise.
+    /// </returns>
+    template<typename IntegerType>
+    inline bool operator==(const IntegerType &nValue) const
+    {
+        return m_value == static_cast<const EQSpaceRelation::EnumType>(nValue);
+    }
+
+
+    /// <summary>
+    /// Equality operator that receives a valid enumeration value.
+    /// </summary>
+    /// <param name="eValue">The enumeration value.</param>
+    /// <returns>
+    /// True if it equals the contained value. False otherwise.
+    /// </returns>
+    bool operator==(const EQIntersections::EnumType &eValue) const
+    {
+        return m_value == eValue;
+    }
+
+
+    /// <summary>
+    /// Casting operator that converts the class capsule into a valid enumeration value.
+    /// </summary>
+    /// <returns>
+    /// The contained enumeration value.
+    /// </returns>
+    inline operator EQIntersections::EnumType() const
+    {
+        return m_value;
+    }
+
+
+    /// <summary>
+    /// Casting operator that converts the enumerated type value into its corresponding integer number.
+    /// </summary>
+    /// <returns>
+    /// The integer number which corresponds to the contained enumeration value.
+    /// </returns>
+    template<typename IntegerType>
+    operator IntegerType() const
+    {
+        return static_cast<IntegerType>(m_value);
+    }
+
+
+    /// <summary>
+    /// Casting operator that converts the enumerated type value into its corresponding name.
+    /// </summary>
+    /// <returns>
+    /// The contained enumeration value name. If the enumeration value is not valid, the returns an empty string.
+    /// </returns>
+    operator const string_q&() const
+    {
+        TNameValueMap::iterator itValueName = EQIntersections::sm_mapValueName.begin();
+        TNameValueMap::const_iterator itValueNameEnd = EQIntersections::sm_mapValueName.end();
+
+        while(itValueName != itValueNameEnd && itValueName->second != m_value)
+            ++itValueName;
+   
+        if(itValueName != itValueNameEnd)
+            return itValueName->first;
+        else
+            return QE_L(""); // [TODO] Thund: Esto debe cambiarse por una constante de QString.
+    }
+
+    /// <summary>
+    /// Converts the enumerated type value into its corresponding name.
+    /// </summary>
+    /// <returns>
+    /// The contained enumeration value name. If the enumeration value is not valid, the returns an empty string.
+    /// </returns>
+    const string_q& ToString()
+    {
+        return *this;
+    }
+
+    // ATTRIBUTES
+	// ---------------
+private:
+
+
+    /// <summary>
+    /// A list of enumeration values with their names.
+    /// </summary>
+    static TNameValuePair sm_arValueName[];
+    
+    /// <summary>
+    /// The dictionary which contains each enumeration value by its name.
+    /// </summary>
+    static TNameValueMap  sm_mapValueName;
+
+    /// <summary>
+    /// The contained enumeration value.
+    /// </summary>
+    EQIntersections::EnumType m_value;
+
+};
+
+
+// CONSTANTS INITIALIZATION
+// ----------------------------
+       
+EQIntersections::TNameValuePair EQIntersections::sm_arValueName[] =
+    {
+        std::pair<string_q, EQIntersections::EnumType>(QE_L("None"),    EQIntersections::E_None),
+        std::pair<string_q, EQIntersections::EnumType>(QE_L("One"), EQIntersections::E_One),
+        std::pair<string_q, EQIntersections::EnumType>(QE_L("Two"), EQIntersections::E_Two),
+        std::pair<string_q, EQIntersections::EnumType>(QE_L("More"), EQIntersections::E_More),
+        std::pair<string_q, EQIntersections::EnumType>(QE_L("Infinite"),    EQIntersections::E_Infinite)
+    };
+
+EQIntersections::TNameValueMap EQIntersections::sm_mapValueName(
+        EQIntersections::sm_arValueName ,
+        &EQIntersections::sm_arValueName[0] + sizeof(EQIntersections::sm_arValueName) / sizeof(EQIntersections::sm_arValueName[0])
+    );
+
+
+
+} //namespace Math
+} //namespace Tools
+} //namespace QuimeraEngine
+} //namespace Kinesis
+
+#endif // __EQINTERSECTIONS__
