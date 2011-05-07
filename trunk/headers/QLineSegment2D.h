89d0b538eb26daa0f7d940aa05f580789018a9d2
andersonjag.blg@gmail.com <andersonjag.blg@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-05-07 13:10:01 +0000

    122 - QLineSegment2D.
    -TODO: Operator=.
    -TODO: Inicialización de constantes UnitLine y LineZero.

---
+++ trunk/headers/QLineSegment2D.h
@@ -0,0 +1,185 @@
+﻿// [TERMS&CONDITIONS]
+
+#ifndef __QLINESEGMENT2D__
+#define __QLINESEGMENT2D__
+
+#include "QLineSegment.h"
+
+#include "QBaseOrb.h"
+
+using namespace Kinesis::QuimeraEngine::Tools::DataTypes;
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace Tools
+{
+namespace Math
+{
+
+// Forward Declarations
+//-----------------------
+class QVector2;
+template<class VectorType> class QBaseTriangle;
+typedef QBaseTriangle<QVector2> QBaseTriangle2;
+class QBaseQuadrilateral;
+
+/// <summary>
+/// This class represents a chunk (segment) of straight line into 2D space, defined by two
+/// endpoints, A and B.
+/// These points will always be expressed as 2D points or vectors.
+/// Please note there's really no "source" and "end" points implicit, that is, it's not an oriented segment
+/// except for the unit line (see below). However, some transforming methods will rotate the segment using A
+/// by default as the pivot point.
+/// </summary>
+class QDllExport QLineSegment2D : public QLineSegment<QVector2>
+{
+
+    // CONSTANTS
+    // ---------------
+public:
+
+    /// <summary>
+    /// Unit segment lying on positive X axis (it's length equals 1).
+    /// </summary>
+	static const QLineSegment2D UnitLine;
+
+    /// <summary>
+    /// Zero segment lying in the coordinates center whose endpoints
+	/// both equals to (0,0) and it's length equals 0.
+    /// </summary>
+	static const QLineSegment2D LineZero;
+
+
+	// CONSTRUCTORS
+	// ---------------
+public:
+
+	/// <summary>
+	/// Default constructor.
+	/// </summary>
+	inline QLineSegment2D() : QLineSegment<QVector2>() { };
+
+	/// <summary>
+    /// Constructor from two vectors.
+    /// </summary>
+    /// <param name="vA">[IN] 2D vector to define endpoint A.</param>
+    /// <param name="vB">[IN] 2D vector to define endpoint B.</param>
+	inline QLineSegment2D (const QVector2& vA, const QVector2& vB) : QLineSegment<QVector2>(vA,vB) { }
+
+	/// <summary>
+    /// Constructor from a 2D line segment.
+    /// </summary>
+    /// <param name="segmt">[IN] 2D line segment containing the two endpoints.</param>
+	inline explicit QLineSegment2D (const QLineSegment<QVector2>& segmt) : QLineSegment<QVector2>(segmt.A, segmt.B)  { }
+
+
+	// METHODS
+	// ---------------
+public:
+	/// <summary>
+	/// Assigning operator.
+	/// </summary>
+	/// <param name="segmt">[IN] The 2D segment to be copied from.</param>
+	/// <returns>
+	/// A reference to the modified line segment.
+	/// </returns>
+    inline QLineSegment2D& operator=(const QBaseLineSegment2& segmt)
+    {
+		// [TODO] It must be done via QLineSegment assign operator,
+		//		  when it's implemented on QLineSegment.
+        //reinterpret_cast<QBaseLineSegment2&>(*this) = segmt;   
+        //return *this;
+    }
+
+	/// <summary>
+	/// This method receives a 2D cirle, and computes whether they intersect each other or not.
+	/// </summary>
+	/// <param name="circle">[IN] The 2D circle to be compared to.</param>
+	/// <returns>
+	/// True if the segment intersects the circle (or if they were either tangent or coincident). Otherwise returns false.
+	/// </returns> 
+	inline bool Intersection (const QBaseOrb<QVector2>& circle) const
+	{
+		// An intersection between the segment and the circle is considered if the minimum
+		// distance between "the whole segment" and the center of the circle (this is, the
+		// minimum distance between the center of the circle and the closest point inside
+		// the segment) is either lesser or equal than the radius of the circle.
+		return QFloat::IsLowerOrEquals(this->MinDistance(circle.P), circle.Radius);
+	};
+
+	/// <summary>
+	/// This method receives a 2D triangle, and computes whether they intersect each other or not.
+	/// </summary>
+	/// <param name="triangl">[IN] The 2D triangle to be compared to.</param>
+	/// <returns>
+	/// True if the segment intersects the triangle (or if they were either tangent or coincident). Otherwise returns false.
+	/// </returns>
+	bool Intersection (const QBaseTriangle2& triangl) const; 
+
+	/// <summary>
+	/// This method receives a 2D quadrilateral, and computes whether they intersect each other or not.
+	/// </summary>
+	/// <param name="quadrl">[IN] The 2D quadrilateral to be compared to.</param>
+	/// <returns>
+	/// True if the segment intersects the quadrilateral (or if they were either tangent or coincident). Otherwise returns false.
+	/// </returns>
+	bool Intersection (const QBaseQuadrilateral& quadrl) const;
+
+	/// <summary>
+	/// This method transforms the 2D segment by rotating an amount defined by a rotation angle. 
+	/// </summary>
+	/// <param name="fRotationAngle">[IN] The angle of rotation.</param>
+	/// <remarks>
+	/// The rotation will be performed using endpoint A as the pivot point for the rotation.
+	/// </remarks>
+	void Transform (const float_q& fRotationAngle);
+
+	/// <summary>
+	/// This method performs a rotation of the 2D segment by rotating an amount defined by a rotation angle
+	/// and then storing the resulting segment in the output parameter.
+	/// </summary>
+	/// <param name="fRotationAngle">[IN] The angle of rotation.</param>
+	/// <param name="segmt">[OUT] It receives the resulting rotated 2D segment.</param>
+	/// <returns>
+	/// The resulting rotated 2D segment.
+	/// </returns>
+	/// <remarks>
+	/// -The rotation will be performed using endpoint A as the pivot point for the rotation.
+	/// -The segment is NOT modified, it stays the same.
+	/// </remarks>
+	void Transform (const float_q& fRotationAngle, QBaseLineSegment2& segmt) const;
+
+	/// <summary>
+	/// This method transforms the 2D segment by rotating an amount defined by a rotation angle. 
+	/// </summary>
+	/// <param name="fRotationAngle">[IN] The angle of rotation.</param>
+	/// <remarks>
+	/// The rotation will be performed using the centre point of the segment as the pivot
+	/// point for the rotation.
+	/// </remarks>
+	void TransformFromCenter (const float_q& fRotationAngle);
+
+	/// <summary>
+	/// This method performs a rotation of the 2D segment by rotating an amount defined by a rotation angle
+	/// and then storing the resulting segment in the output parameter.
+	/// </summary>
+	/// <param name="fRotationAngle">[IN] The angle of rotation.</param>
+	/// <param name="segmt">[OUT] It receives the resulting rotated 2D segment.</param>
+	/// <returns>
+	/// The resulting rotated 2D segment.
+	/// </returns>
+	/// <remarks>
+	/// -The rotation will be performed using the centre point of the segment as the pivot point for the rotation.
+	/// -The segment is NOT modified, it stays the same.
+	/// </remarks>
+	void TransformFromCenter (const float_q& fRotationAngle, QBaseLineSegment2& segmt) const;
+};
+
+} //namespace Math
+} //namespace Tools
+} //namespace QuimeraEngine
+} //namespace Kinesis
+
+#endif // __QLINESEGMENT2D__
---
+++ trunk/source/QLineSegment2D.cpp
@@ -0,0 +1,175 @@
+// [TERMS&CONDITIONS]
+
+#include "QBaseTriangle.h"
+#include "QBaseQuadrilateral.h"
+#include "QTransformationMatrix3x3.h"
+
+#include "QLineSegment2D.h"
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace Tools
+{
+namespace Math
+{
+
+//##################=======================================================##################
+//##################             ____________________________              ##################
+//##################            |                            |             ##################
+//##################            |  CONSTANTS INITIALIZATION  |             ##################
+//##################           /|                            |\            ##################
+//##################             \/\/\/\/\/\/\/\/\/\/\/\/\/\/              ##################
+//##################                                                       ##################
+//##################=======================================================##################
+const QLineSegment2D QLineSegment2D::UnitLine(QVector2::ZeroVector, QVector2::UnitVectorX);
+const QLineSegment2D QLineSegment2D::LineZero(QVector2::ZeroVector, QVector2::ZeroVector);
+
+
+//##################=======================================================##################
+//##################			 ____________________________			   ##################
+//##################			|							 |			   ##################
+//##################		    |		    METHODS			 |			   ##################
+//##################		   /|							 |\			   ##################
+//##################			 \/\/\/\/\/\/\/\/\/\/\/\/\/\/			   ##################
+//##################													   ##################
+//##################=======================================================##################
+
+bool QLineSegment2D::Intersection (const QBaseTriangle2& triangl) const
+{
+	// Just checking if the segment instersects with any edge of the triangle.
+
+	QLineSegment<QVector2> lsAB(triangl.A, triangl.B);
+	QLineSegment<QVector2> lsBC(triangl.B, triangl.C);
+	QLineSegment<QVector2> lsCA(triangl.C, triangl.A);
+
+	return     ( QLineSegment<QVector2>::Intersection(lsAB) )
+			|| ( QLineSegment<QVector2>::Intersection(lsBC) )
+			|| ( QLineSegment<QVector2>::Intersection(lsCA) );
+};
+
+bool QLineSegment2D::Intersection (const QBaseQuadrilateral& quadrl) const
+{
+	// Just checking if the segment instersects with any edge of the quadrilateral.
+
+	QLineSegment<QVector2> lsAB(quadrl.A, quadrl.B);
+	QLineSegment<QVector2> lsBC(quadrl.B, quadrl.C);
+	QLineSegment<QVector2> lsCD(quadrl.C, quadrl.D);
+	QLineSegment<QVector2> lsDA(quadrl.D, quadrl.A);
+
+	return     ( QLineSegment<QVector2>::Intersection(lsAB) )
+			|| ( QLineSegment<QVector2>::Intersection(lsBC) )
+			|| ( QLineSegment<QVector2>::Intersection(lsCD) )
+			|| ( QLineSegment<QVector2>::Intersection(lsDA) );
+};
+
+void QLineSegment2D::Transform (const float_q& fRotationAngle)
+{
+	// Matrix for rotating a 2D point (expressed in local coordinates) around the coordinate
+	// axis, and then displaced an amount across X and Y (for expressing the point finally in
+	// world coordinates) indicated by A, as being the pivot point.
+	QTransformationMatrix3x3 m(QBaseVector2(A), fRotationAngle, QBaseVector2(QFloat::_1));
+
+	// STEP 1) Substracting A from B we're expressing B in LOCAL coordinates.
+	//         (** by the same way, if we substract A from A we'll have A in 
+	//		       the center of coordinates axis, as being A to become the
+	//			   pivot point **)
+	QBaseVector2 vBLocal(B.x - A.x, B.y - A.y);
+
+	// STEP 2) Now we transform B; please note due to A is the pivot point, 
+	//         transforming B is only needed.
+	B.x = vBLocal.x * m.ij[0][0] + vBLocal.y * m.ij[1][0] +  m.ij[2][0];
+	B.y = vBLocal.x * m.ij[0][1] + vBLocal.y * m.ij[1][1] +  m.ij[2][1];
+
+	// After STEP 2) we have the segment rotated around A and expressed again in
+	// WORLD coordinates.
+};
+
+void QLineSegment2D::Transform (const float_q& fRotationAngle, QBaseLineSegment<QVector2>& segmt) const
+{
+	// Matrix for rotating a 2D point (expressed in local coordinates) around the coordinate
+	// axis, and then displaced an amount across X and Y (for expressing the point finally in
+	// world coordinates) indicated by A, as being the pivot point.
+	QTransformationMatrix3x3 m(QBaseVector2(A), fRotationAngle, QBaseVector2(QFloat::_1));
+
+	// STEP 1) Substracting A from B we're expressing B in LOCAL coordinates.
+	//         (** by the same way, if we substract A from A we'll have A in 
+	//		       the center of coordinates axis, as being A to become the
+	//			   pivot point **)
+	QBaseVector2 vBLocal(B.x - A.x, B.y - A.y);
+
+	// STEP 2) A is the pivot point, so copying A is just needed.
+	segmt.A = A;
+
+	// STEP 3) Now we transform and copy B; please note due to A is the pivot point, 
+	//         transforming B is only needed.
+	segmt.B.x = vBLocal.x * m.ij[0][0] + vBLocal.y * m.ij[1][0] +  m.ij[2][0];
+	segmt.B.y = vBLocal.x * m.ij[0][1] + vBLocal.y * m.ij[1][1] +  m.ij[2][1];
+
+	// So after STEP 3) we have in the output parameter the segment rotated around A
+	//	and expressed again in WORLD coordinates.
+};
+
+void QLineSegment2D::TransformFromCenter (const float_q& fRotationAngle)
+{
+	// STEP 1.1) First of all we get the segment's centre.
+	QVector2 vCenter;
+	this->GetCenter(vCenter); 
+							  
+	// STEP 1.2) Creating a matrix for rotating a 2D point (expressed in local coordinates) around the coordinate
+	//           axis, and then displaced an amount across X and Y (for expressing the point finally in
+	//           world coordinates) indicated by the segment's centre, as being the pivot point.
+	QTransformationMatrix3x3 m(vCenter, fRotationAngle, QBaseVector2(QFloat::_1));
+
+	// STEP 2) Substracting the segment's centre from B (A) we're expressing
+	//		   B (A) in LOCAL coordinates.
+	//         (** by the same way, if we substract the centre from itself we'll
+	//			   have the centre in the center of coordinates axis, as being
+	//			   the centre to become the pivot point **)
+	QBaseVector2 vALocal(A.x - vCenter.x, A.y - vCenter.y);
+	QBaseVector2 vBLocal(B.x - vCenter.x, B.y - vCenter.y);
+
+	// STEP 3) Now we transform A and B.
+	A.x = vALocal.x * m.ij[0][0] + vALocal.y * m.ij[1][0] +  m.ij[2][0];
+	A.y = vALocal.x * m.ij[0][1] + vALocal.y * m.ij[1][1] +  m.ij[2][1];
+	B.x = vBLocal.x * m.ij[0][0] + vBLocal.y * m.ij[1][0] +  m.ij[2][0];
+	B.y = vBLocal.x * m.ij[0][1] + vBLocal.y * m.ij[1][1] +  m.ij[2][1];
+
+	// After STEP 3) we have the segment rotated around the center and expressed
+	// again in WORLD coordinates.
+};
+
+void QLineSegment2D::TransformFromCenter (const float_q& fRotationAngle, QBaseLineSegment<QVector2>& segmt) const
+{
+	// STEP 1.1) First of all we get the segment's centre.
+	QVector2 vCenter;
+	this->GetCenter(vCenter);
+
+	// STEP 1.2) Creating a matrix for rotating a 2D point (expressed in local coordinates) around the coordinate
+	//           axis, and then displaced an amount across X and Y (for expressing the point finally in
+	//           world coordinates) indicated by the segment's centre, as being the pivot point.
+	QTransformationMatrix3x3 m(vCenter, fRotationAngle, QBaseVector2(QFloat::_1));
+
+	// STEP 2) Substracting the segment's centre from B (A) we're expressing
+	//		   B (A) in LOCAL coordinates.
+	//         (** by the same way, if we substract the centre from itself we'll
+	//			   have the centre in the center of coordinates axis, as being
+	//			   the centre to become the pivot point **)
+	QBaseVector2 vALocal(A.x - vCenter.x, A.y - vCenter.y);
+	QBaseVector2 vBLocal(B.x - vCenter.x, B.y - vCenter.y);
+
+	// STEP 3) Now we transform A and B.
+	segmt.A.x = vALocal.x * m.ij[0][0] + vALocal.y * m.ij[1][0] +  m.ij[2][0];
+	segmt.A.y = vALocal.x * m.ij[0][1] + vALocal.y * m.ij[1][1] +  m.ij[2][1];
+	segmt.B.x = vBLocal.x * m.ij[0][0] + vBLocal.y * m.ij[1][0] +  m.ij[2][0];
+	segmt.B.y = vBLocal.x * m.ij[0][1] + vBLocal.y * m.ij[1][1] +  m.ij[2][1];
+
+	// After STEP 3) we have the segment rotated around the center and expressed
+	// again in WORLD coordinates.
+};
+
+} //namespace Math
+} //namespace Tools
+} //namespace QuimeraEngine
+} //namespace Kinesis
