e8b2ea6d841a9bc8b9f2a3ec7abf76e578f971a0
jwladi@gmail.com <jwladi@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-06-12 06:59:10 +0000

    133-QPlane Extension 002 Added some functionalities

--- trunk/source/QPlane.cpp
+++ trunk/source/QPlane.cpp
@@ -52,6 +52,32 @@ QPlane QPlane::operator / (const float_q &fValue) const
     return QPlane(this->a / fValue, this->b / fValue, this->c / fValue, this->d / fValue);
 }
 
+EQSpaceRelation QPlane::SpaceRelation(const QBasePlane &p) const
+{
+    // Cross product.
+    if (QFloat::IsZero(p.b*this->c - p.c*this->b) && 
+        QFloat::IsZero(p.c*this->a - p.a*this->c) && 
+        QFloat::IsZero(p.a*this->b - p.b*this->a))
+    {
+
+        QPlane plAux1(p), plAux2(*this);
+
+        plAux1.Normalize();
+        plAux2.Normalize();
+
+        // They are the same plane.
+        if (plAux1 == plAux2)
+            return EQSpaceRelation::E_Contained;
+        // They are parallel planes.
+        else if (QFloat::IsNegative(plAux1.d - plAux2.d))
+            return EQSpaceRelation::E_NegativeSide;
+        else
+            return EQSpaceRelation::E_PositiveSide;
+    }
+    else
+        return EQSpaceRelation::E_BothSides; 
+}
+
 } //namespace Math
 } //namespace Tools
 } //namespace QuimeraEngine
