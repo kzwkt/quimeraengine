1fe4d8a7d7b546b971ba1a304ce0d521a53572ef
lince3d <lince3d@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-08-01 17:38:20 +0000

    Problema de linkado relacionado con inicialización de miembros estáticos en el header en lugar de en el archivo de implementación. Las clases de enumerado ahora están partidas en 2.

--- headers/EQIntersections.h
+++ headers/EQIntersections.h
@@ -246,26 +246,6 @@ private:
 
 };
 
-
-// CONSTANTS INITIALIZATION
-// ----------------------------
-       
-EQIntersections::TNameValuePair EQIntersections::sm_arValueName[] =
-    {
-        std::pair<string_q, EQIntersections::EnumType>(QE_L("None"),    EQIntersections::E_None),
-        std::pair<string_q, EQIntersections::EnumType>(QE_L("One"), EQIntersections::E_One),
-        std::pair<string_q, EQIntersections::EnumType>(QE_L("Two"), EQIntersections::E_Two),
-        std::pair<string_q, EQIntersections::EnumType>(QE_L("More"), EQIntersections::E_More),
-        std::pair<string_q, EQIntersections::EnumType>(QE_L("Infinite"),    EQIntersections::E_Infinite)
-    };
-
-EQIntersections::TNameValueMap EQIntersections::sm_mapValueName(
-        EQIntersections::sm_arValueName ,
-        &EQIntersections::sm_arValueName[0] + sizeof(EQIntersections::sm_arValueName) / sizeof(EQIntersections::sm_arValueName[0])
-    );
-
-
-
 } //namespace Math
 } //namespace Tools
 } //namespace QuimeraEngine
--- headers/EQSpaceRelation.h
+++ headers/EQSpaceRelation.h
@@ -241,25 +241,6 @@ private:
 
 };
 
-
-// CONSTANTS INITIALIZATION
-// ----------------------------
-
-EQSpaceRelation::TNameValuePair EQSpaceRelation::sm_arValueName[] =
-    {
-        std::pair<string_q, EQSpaceRelation::EnumType>(QE_L("Contained"),    EQSpaceRelation::E_Contained),
-        std::pair<string_q, EQSpaceRelation::EnumType>(QE_L("PositiveSide"), EQSpaceRelation::E_PositiveSide),
-        std::pair<string_q, EQSpaceRelation::EnumType>(QE_L("NegativeSide"), EQSpaceRelation::E_NegativeSide),
-        std::pair<string_q, EQSpaceRelation::EnumType>(QE_L("BothSides"),    EQSpaceRelation::E_BothSides)
-    };
-
-EQSpaceRelation::TNameValueMap EQSpaceRelation::sm_mapValueName(
-        EQSpaceRelation::sm_arValueName ,
-        &EQSpaceRelation::sm_arValueName[0] + sizeof(EQSpaceRelation::sm_arValueName) / sizeof(EQSpaceRelation::sm_arValueName[0])
-    );
-
-
-
 } //namespace Math
 } //namespace Tools
 } //namespace QuimeraEngine
