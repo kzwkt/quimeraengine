4ada3505bef320215fd2804e23474ecd169c8800
andersonjag.blg@gmail.com <andersonjag.blg@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-09-17 16:03:42 +0000

    -Realizadas las correcciones en QVector3 y QVector4 ::CrossProduct.
    -Especificada en la documentación de Cross Product que el vector residente es el primero en la operación Producto Vectorial, y el que es parámetro de entrada es el segundo.

--- trunk/headers/QVector3.h
+++ trunk/headers/QVector3.h
@@ -31,8 +31,8 @@ class QSpaceConversionMatrix;
 
 /// <summary>
 /// This class implements three components vector functionality. It inherits from QBaseVector3.
-/// A vector is a geometric object that has both a magnitude (or length) and direction. 
-/// It is frequently represented by a line segment with a definite direction, or graphically as an arrow, 
+/// A vector is a geometric object that has both a magnitude (or length) and direction.
+/// It is frequently represented by a line segment with a definite direction, or graphically as an arrow,
 /// connecting an initial point A with a terminal point B. The vector is then denoted by AB with an arrow hat.
 /// </summary>
 class QDllExport QVector3 : public QBaseVector3
@@ -123,7 +123,7 @@ public:
     /// </summary>
     /// <param name="fValue">[IN] Floating point value for all components.</param>
     inline explicit QVector3(const float_q &fValue) : QBaseVector3(fValue) { }
-    
+
     /// <summary>
     /// Constructor from a three component array of floating point values.
     /// </summary>
@@ -213,7 +213,7 @@ public:
     /// A vector that is the result of the division.
     /// </returns>
     QVector3 operator / (const float_q &fValue) const;
-        
+
     /// <summary>
     /// Division by a vector: it's performed component by component.
     /// </summary>
@@ -222,7 +222,7 @@ public:
     /// A vector that is the result of the division.
     /// </returns>
     QVector3 operator / (const QBaseVector3 &v) const;
-    
+
     /// <summary>
     /// Compares two vectors.
     /// </summary>
@@ -256,13 +256,13 @@ public:
     /// <returns>
     /// A reference to vector result of the addition.
     /// </returns>
-    inline QVector3& operator += (const QBaseVector3 &v) 
-    { 
+    inline QVector3& operator += (const QBaseVector3 &v)
+    {
         this->x += v.x;
         this->y += v.y;
-        this->z += v.z; 
-        
-        return *this; 
+        this->z += v.z;
+
+        return *this;
     }
 
     /// <summary>
@@ -272,13 +272,13 @@ public:
     /// <returns>
     /// A reference to vector result of the subtraction.
     /// </returns>
-    inline QVector3& operator -= (const QBaseVector3 &v) 
-    { 
-        this->x -= v.x; 
-        this->y -= v.y; 
-        this->z -= v.z; 
-        
-        return *this; 
+    inline QVector3& operator -= (const QBaseVector3 &v)
+    {
+        this->x -= v.x;
+        this->y -= v.y;
+        this->z -= v.z;
+
+        return *this;
     }
 
     /// <summary>
@@ -288,12 +288,12 @@ public:
     /// <returns>
     /// A reference to vector result of the product.
     /// </returns>
-    inline QVector3& operator *= (const float_q &fValue) 
-    { 
-        this->x *= fValue; 
-        this->y *= fValue; 
-        this->z *= fValue; 
-        
+    inline QVector3& operator *= (const float_q &fValue)
+    {
+        this->x *= fValue;
+        this->y *= fValue;
+        this->z *= fValue;
+
         return *this;
     }
 
@@ -304,13 +304,13 @@ public:
     /// <returns>
     /// A reference to vector result of the product.
     /// </returns>
-    inline QVector3& operator *= (const QBaseVector3 &v) 
-    { 
-        this->x *= v.x; 
-        this->y *= v.y; 
-        this->z *= v.z; 
-        
-        return *this; 
+    inline QVector3& operator *= (const QBaseVector3 &v)
+    {
+        this->x *= v.x;
+        this->y *= v.y;
+        this->z *= v.z;
+
+        return *this;
     }
 
     /// <summary>
@@ -329,18 +329,18 @@ public:
     /// <returns>
     /// A reference to vector result of the division.
     /// </returns>
-    inline QVector3& operator /= (const float_q &fValue) 
-    { 
+    inline QVector3& operator /= (const float_q &fValue)
+    {
         // Checkout to avoid division by 0
         QE_ASSERT(fValue != QFloat::_0);
 
-        this->x /= fValue; 
-        this->y /= fValue; 
-        this->z /= fValue; 
-        
-        return *this; 
+        this->x /= fValue;
+        this->y /= fValue;
+        this->z /= fValue;
+
+        return *this;
     }
-    
+
     /// <summary>
     /// Divides resident vector by a vector provided. It's performed component by component.
     /// </summary>
@@ -353,10 +353,10 @@ public:
         // Checkout to avoid division by 0
         QE_ASSERT (v.x != QFloat::_0 && v.y != QFloat::_0 && v.z != QFloat::_0);
 
-        this->x /= v.x; 
-        this->y /= v.y; 
-        this->z /= v.z; 
-        
+        this->x /= v.x;
+        this->y /= v.y;
+        this->z /= v.z;
+
         return *this;
     }
 
@@ -371,7 +371,7 @@ public:
     /// </returns>
     inline QVector3& operator=(const QBaseVector3 &v)
     {
-        reinterpret_cast<QBaseVector3&>(*this) = v;    
+        reinterpret_cast<QBaseVector3&>(*this) = v;
         return *this;
     }
 
@@ -392,8 +392,8 @@ public:
     /// A floating point value which is the length of the vector.
     /// </returns>
     inline float_q GetLength() const
-    { 
-        return sqrt(this->x*this->x + this->y*this->y + this->z*this->z); 
+    {
+        return sqrt(this->x*this->x + this->y*this->y + this->z*this->z);
     }
 
     /// <summary>
@@ -403,24 +403,24 @@ public:
     /// A floating point value which is the squared length of the vector.
     /// </returns>
     inline float_q GetSquaredLength() const
-    { 
-        return this->x*this->x + this->y*this->y + this->z*this->z; 
+    {
+        return this->x*this->x + this->y*this->y + this->z*this->z;
     }
 
     /// <summary>
     /// Makes resident vector unitary.
     /// </summary>
-    inline void Normalize() 
+    inline void Normalize()
     {
         // Gets vector length
-        float_q fLength = this->GetLength(); 
+        float_q fLength = this->GetLength();
 
         // Checkout to avoid division by 0
         QE_ASSERT(fLength != QFloat::_0);
 
         //Normalize
-        this->x /= fLength; 
-        this->y /= fLength;    
+        this->x /= fLength;
+        this->y /= fLength;
         this->z /= fLength;
     }
 
@@ -437,11 +437,11 @@ public:
     /// <summary>
     /// Convert resident vector in its opposite vector.
     /// </summary>
-    inline void Reverse() 
-    { 
-        this->x = -this->x; 
-        this->y = -this->y; 
-        this->z = -this->z; 
+    inline void Reverse()
+    {
+        this->x = -this->x;
+        this->y = -this->y;
+        this->z = -this->z;
     }
 
     /// <summary>
@@ -457,21 +457,21 @@ public:
     /// <summary>
     /// Resets all components of resident vector to 1.
     /// </summary>
-    inline void ResetToOne() 
-    { 
-        this->x = QFloat::_1; 
-        this->y = QFloat::_1; 
-        this->z = QFloat::_1; 
+    inline void ResetToOne()
+    {
+        this->x = QFloat::_1;
+        this->y = QFloat::_1;
+        this->z = QFloat::_1;
     }
 
     /// <summary>
     /// Resets all components of resident vector to 0.
     /// </summary>
-    inline void ResetToZero() 
-    { 
-        this->x = QFloat::_0; 
-        this->y = QFloat::_0; 
-        this->z = QFloat::_0; 
+    inline void ResetToZero()
+    {
+        this->x = QFloat::_0;
+        this->y = QFloat::_0;
+        this->z = QFloat::_0;
     }
 
     /// <summary>
@@ -481,8 +481,8 @@ public:
     /// True if all components are 0. False otherwise.
     /// </returns>
     inline bool IsZero() const
-    { 
-        return QFloat::IsZero(this->x) && QFloat::IsZero(this->y) && QFloat::IsZero(this->z); 
+    {
+        return QFloat::IsZero(this->x) && QFloat::IsZero(this->y) && QFloat::IsZero(this->z);
     }
 
     /// <summary>
@@ -492,8 +492,8 @@ public:
     /// True if all components are 1. False otherwise.
     /// </returns>
     inline bool IsVectorOfOnes() const
-    { 
-        return QFloat::AreEquals(this->x, QFloat::_1) && QFloat::AreEquals(this->y, QFloat::_1) && QFloat::AreEquals(this->z, QFloat::_1); 
+    {
+        return QFloat::AreEquals(this->x, QFloat::_1) && QFloat::AreEquals(this->y, QFloat::_1) && QFloat::AreEquals(this->z, QFloat::_1);
     }
 
     /// <summary>
@@ -504,8 +504,8 @@ public:
     /// A floating point value which is the result of Dot Product.
     /// </returns>
     inline float_q DotProduct(const QBaseVector3 &v) const
-    { 
-        return(this->x*v.x + this->y*v.y + this->z*v.z); 
+    {
+        return(this->x*v.x + this->y*v.y + this->z*v.z);
     }
 
     /// <summary>
@@ -516,9 +516,9 @@ public:
     /// A floating point value which is the smaller angle between vectors (less or equal 180º).
     /// </returns>
     inline float_q DotProductAngle(const QVector3 &v) const
-    { 
+    {
         float_q fLength = this->GetLength() * v.GetLength();
-        
+
         // Checkout to avoid division by zero.
         QE_ASSERT(fLength != QFloat::_0);
 
@@ -541,12 +541,16 @@ public:
     /// </summary>
     /// <param name="v">[IN] Multiplying vector.</param>
     void CrossProduct(const QBaseVector3 &v);
-    
+
     /// <summary>
     /// Calculates Cross Product by a vector provided (left handed rules).
     /// </summary>
     /// <param name="v">[IN] Multiplying vector.</param>
     /// <param name="vOut">[OUT] Vector to store the resul of Cross Product.</param>
+    /// <remarks>
+    /// Cross Product is noncommutative, please note the resident vector is the first operator, set to the left
+    /// of the Cross Product operator sign, and the input vector is the second operator (set to the right).
+    /// </remarks>
     inline void CrossProduct(const QBaseVector3 &v, QBaseVector3 &vOut) const
     {
         vOut = *this;
@@ -559,10 +563,10 @@ public:
     /// <param name="fFactor">[IN] A floating point value which represents how close is the result vector from the resident vector (per one).</param>
     /// <param name="v">[IN] Vector with which to interpolate.</param>
     inline void Lerp(const float_q &fFactor, const QBaseVector3 &v)
-    { 
+    {
         this->x = this->x*fFactor + v.x*(QFloat::_1 - fFactor);
         this->y = this->y*fFactor + v.y*(QFloat::_1 - fFactor);
-        this->z = this->z*fFactor + v.z*(QFloat::_1 - fFactor); 
+        this->z = this->z*fFactor + v.z*(QFloat::_1 - fFactor);
     }
 
     /// <summary>
@@ -595,18 +599,18 @@ public:
     /// <summary>
     /// Applies a rotation quaternion to resident vector. The rotation applied is clockwise.
     /// It's made by multiplying resident vector by the quaternion provided, as follows:
-    /// \f$ v' = Q \cdot v \cdot Q^*\f$, 
+    /// \f$ v' = Q \cdot v \cdot Q^*\f$,
     /// where v is the resident vector, Q the quaternion and \f$ Q^*\f$ the conjugate of the quaternion.
     /// [TODO] jwladi: probably it will be necessary verify if rotation is clockwise or not.
     /// </summary>
     /// <param name="qR">[IN] The rotation quaternion.</param>
     void Transform(const QQuaternion &qR);
-    
+
     /// <summary>
     /// Applies a rotation quaternion to resident vector, storing results in an out vector provided.
     /// The rotation applied is clockwise.
     /// It's made by multiplying resident vector by the quaternion provided, as follows:
-    /// \f$ v' = Q \cdot v \cdot Q^*\f$, 
+    /// \f$ v' = Q \cdot v \cdot Q^*\f$,
     /// where v is the resident vector, Q the quaternion and \f$ Q^*\f$ the conjugate of the quaternion.
     /// </summary>
     /// <param name="qR">[IN] The rotation quaternion.</param>
@@ -625,7 +629,7 @@ public:
     /// </summary>
     /// <param name="dqTransf">[IN] The dual quaternion which wears the transformation.</param>
     void Transform(const QDualQuaternion &dqTransf);
-    
+
     /// <summary>
     /// Applies a rigid transformation dual quaternion to resident vector, storing results in an out vector provided.
     /// It's made by multiplying resident vector by the dual quaternion provided, as follows:
@@ -641,7 +645,7 @@ public:
     }
 
     /// <summary>
-    /// Applies a rotation to resident vector, multiplying the vector by a rotation matrix 
+    /// Applies a rotation to resident vector, multiplying the vector by a rotation matrix
     /// to transform it.
     /// </summary>
     /// <param name="mRot">[IN] The rotation matrix.</param>
@@ -649,9 +653,9 @@ public:
 	{
 		*this *= mRot;
 	}
- 
+
     /// <summary>
-    /// Applies a rotation to resident vector, multiplying the vector by a rotation matrix 
+    /// Applies a rotation to resident vector, multiplying the vector by a rotation matrix
     /// to transform it. The rotated vector is stored into the provided one.
     /// </summary>
     /// <param name="mRot">[IN] The rotation matrix.</param>
@@ -663,7 +667,7 @@ public:
 	}
 
     /// <summary>
-    /// Applies a scale transformation to resident vector, multiplying the vector by a scale matrix 
+    /// Applies a scale transformation to resident vector, multiplying the vector by a scale matrix
     /// to transform it.
     /// </summary>
     /// <param name="mScale">[IN] The scale matrix.</param>
@@ -675,7 +679,7 @@ public:
 	}
 
     /// <summary>
-    /// Applies a scale transformation to resident vector, multiplying the vector by a scale matrix 
+    /// Applies a scale transformation to resident vector, multiplying the vector by a scale matrix
     /// to transform it. The scaled vector is stored into the provided one.
     /// </summary>
     /// <param name="mScale">[IN] The scale matrix.</param>
@@ -687,7 +691,7 @@ public:
 	}
 
     /// <summary>
-    /// Applies a translation to resident vector, multiplying the vector by a translation matrix 
+    /// Applies a translation to resident vector, multiplying the vector by a translation matrix
 	/// to transform it. We assume that resident vector represents a 3D point
 	/// \f$(v_x, v_y, v_z, 1)\f$ and not a 3D vector \f$(v_x, v_y, v_z, 0)\f$,
 	/// since a 3D vector cannot be displaced.
@@ -702,7 +706,7 @@ public:
 	}
 
     /// <summary>
-    /// Applies a translation to resident vector, multiplying the vector by a translation matrix 
+    /// Applies a translation to resident vector, multiplying the vector by a translation matrix
 	/// to transform it. We assume that resident vector represents a 3D point
 	/// \f$(v_x, v_y, v_z, 1)\f$ and not a 3D vector \f$(v_x, v_y, v_z, 0)\f$,
 	/// since a 3D vector cannot be displaced. The translated vector is stored into the provided one.
@@ -718,7 +722,7 @@ public:
 
     /// <summary>
     /// Applies a transformation composed of a scale, a rotation and a translation
-	/// to resident vector, multiplying the vector by a transformation matrix 
+	/// to resident vector, multiplying the vector by a transformation matrix
 	/// to transform it. We assume that resident vector represents a 3D point
 	/// \f$(v_x, v_y, v_z, 1)\f$ and not a 3D vector \f$(v_x, v_y, v_z, 0)\f$,
 	/// since a 3D vector cannot be displaced.
@@ -732,13 +736,13 @@ public:
 		vAux.x = this->x * mTransf.ij[0][0] + this->y * mTransf.ij[1][0] + this->z * mTransf.ij[2][0] + mTransf.ij[3][0];
         vAux.y = this->x * mTransf.ij[0][1] + this->y * mTransf.ij[1][1] + this->z * mTransf.ij[2][1] + mTransf.ij[3][1];
         vAux.z = this->x * mTransf.ij[0][2] + this->y * mTransf.ij[1][2] + this->z * mTransf.ij[2][2] + mTransf.ij[3][2];
-        
+
         *this = vAux;
     }
 
     /// <summary>
     /// Applies a transformation composed of a scale, a rotation and a translation
-	/// to resident vector, multiplying the vector by a transformation matrix 
+	/// to resident vector, multiplying the vector by a transformation matrix
 	/// to transform it. We assume that resident vector represents a 3D point
 	/// \f$(v_x, v_y, v_z, 1)\f$ and not a 3D vector \f$(v_x, v_y, v_z, 0)\f$,
 	/// since a 3D vector cannot be displaced. The transformed vector is stored into the provided one.
@@ -754,7 +758,7 @@ public:
 
     /// <summary>
     /// Applies a transformation composed of a scale, a rotation and a translation
-	/// to resident vector, multiplying the vector by a space conversion matrix 
+	/// to resident vector, multiplying the vector by a space conversion matrix
 	/// to transform it. We assume that resident vector represents a 3D point
 	/// \f$(v_x, v_y, v_z, 1)\f$ and not a 3D vector \f$(v_x, v_y, v_z, 0)\f$,
 	/// since a 3D vector cannot be displaced.
@@ -764,7 +768,7 @@ public:
 
 	/// <summary>
     /// Applies a transformation composed of a scale, a rotation and a translation
-	/// to resident vector, multiplying the vector by a space conversion matrix 
+	/// to resident vector, multiplying the vector by a space conversion matrix
 	/// to transform it. We assume that resident vector represents a 3D point
 	/// \f$(v_x, v_y, v_z, 1)\f$ and not a 3D vector \f$(v_x, v_y, v_z, 0)\f$,
 	/// since a 3D vector cannot be displaced. The transformed vector is stored into the provided one.
@@ -784,8 +788,8 @@ public:
     /// <returns>The std::string with the format specified.</returns>
     inline string_q ToString() const
     {
-        return QE_L("V3(") + QFloat::ToString(this->x) + 
-               QE_L(", ")  + QFloat::ToString(this->y) + 
+        return QE_L("V3(") + QFloat::ToString(this->x) +
+               QE_L(", ")  + QFloat::ToString(this->y) +
                QE_L(", ")  + QFloat::ToString(this->z) + QE_L(")");
     }
 };
