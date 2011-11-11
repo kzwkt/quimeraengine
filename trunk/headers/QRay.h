2c48eb0e1f09674a273cc398f8080f09c10dcb39
jwladi@gmail.com <jwladi@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-11-11 22:21:13 +0000

    138 - QRay3D Implements the functionality of a ray in 3D space. Adds Intersection and IntersectionPoint methods which accepts a QBaseOrb to QRay.h

--- trunk/headers/QRay.h
+++ trunk/headers/QRay.h
@@ -4,6 +4,7 @@
 #define __QRAY__
 
 #include "QBaseRay.h"
+#include "QBaseOrb.h"
 #include "EQIntersections.h"
 
 using namespace Kinesis::QuimeraEngine::Tools::DataTypes;
@@ -127,6 +128,150 @@ public:
         vRayPoint = this->Direction;
         vRayPoint *= fDistance;
         vRayPoint += this->Point;
+    }
+
+    /// <summary>
+    /// Checks if resident ray intersects with the provided orb. Ray must be normalized to ensure correct result.
+    /// </summary>
+    /// <param name="orb">[IN] The orb whose intersection with resident ray will be checked.</param>
+    /// <returns>
+    /// True if ray intersect orb, false otherwise.
+    /// </returns>
+    inline bool Intersection (const QBaseOrb<VectorTypePoint> &orb) const
+    {
+        // Converts all vectors to VectorTypeDirection, that always will be QVector2 or QVector3
+        VectorTypeDirection vNewRayPoint(this->Point - orb.P);
+
+        //  B/2 (instead of B) is calculed here to optimize comparison.
+        const float_q &fB = vNewRayPoint.DotProduct(this->Direction);
+
+        // Since ray is normalized, A = 1.
+        //const float_q &fA = this->Direction.DotProduct(this->Direction);
+
+        const float_q &fC = vNewRayPoint.DotProduct(vNewRayPoint) - orb.Radius * orb.Radius;
+
+        // Discriminant: B^2 - 4AC
+        const float_q fD = fB*fB - fC;
+
+        if (QFloat::IsNegative(fD)) // D = B^2 - 4AC < 0 => 0 intersections
+            return false;
+        else if ( QFloat::IsZero(fD) ) // D = B^2 - 4AC = 0 => 1 intersection
+        {
+            if ( QFloat::IsPositive(-fB) ) // T = -B/2A -> must be >= 0
+                return true;
+            else
+                return false;
+        }
+        else
+            return QFloat::IsLowerOrEquals(fB, sqrt(fD));
+
+            // Explanation:
+
+            // In this case, there are two posible intersections:
+            //    T1 = -fB - sqrt(fD)   from (-B - sqrt(B*B - 4AC))/2A, where A = 1, B = 2*fB
+            //    T2 = -fB + sqrt(fD)   from (-B + sqrt(B*B - 4AC))/2A, where A = 1, B = 2*fB
+            //
+            // One of T1 and T2 must be positive:
+            // T1 >= 0  => fB <= -sqrt(fD)
+            //    OR
+            // T2 >= 0 => fB <= sqrt(fD)
+            //
+            // That is, is sufficient that fB <= sqrt(fD)
+    }
+
+    /// <summary>
+	/// Computes the intersection point between resident ray and provided orb, if it exists.
+	/// Ray must be normalized to ensure correct result.
+	/// </summary>
+	/// <param name="orb">[IN] The orb whose intersection with resident ray will be checked.</param>
+	/// <param name="vPoint1">[OUT] First point where they intersect, if they do.</param>
+	/// <param name="vPoint2">[OUT] Second point where they intersect, if they do.</param>
+	/// <returns>
+    /// An enumerated value which represents the number of intersections between the ray and the orb, and can take
+    /// the following values: E_None, E_One and E_Two.
+	/// </returns>
+	/// <remarks>
+	/// -If there's no intersection point, the output parameters won't be modified.
+	/// -If there's one intersection point, the second output parameter won't be modified,
+	/// and first output parameter is filled with the intersection point.
+    /// -If there are two intersection points, both output parameters are filled with the intersection points, storing
+    /// in the first output parameter the closest to the origin point of the ray.
+	/// </remarks>
+    EQIntersections IntersectionPoint(const QBaseOrb<VectorTypePoint> &orb, VectorTypePoint &vPoint1, VectorTypePoint &vPoint2) const
+    {
+        // We set all vectors to the same type that output parameters to allow operations
+        const VectorTypePoint &vDirection(this->Direction);
+
+        // We reduce ray and orb to origin, in order to simplify orb equation, and we calculate
+        // the new ray origin point
+        const VectorTypePoint &vNewRayPoint(this->Point - orb.P);
+
+        // We replace then in the orb equation to force it to verify the ray equation
+        // vDirection^2*T^2 + 2*vNewA*vDirection*T + vNewA^2 - r^2 = 0
+        // The intersections with ray follows this rules (extracted from quadratic ecuation):
+        //       D = B^2 - 4AC = 0 => 1 intersection
+        //       D = B^2 - 4AC < 0 => 0 intersections
+        //       D = B^2 - 4AC > 0 => 2 intersections
+
+        // Since ray is normalized, A = 1
+        // const float_q &fA = this->Direction.DotProduct(this->Direction);
+
+        const float_q &fB = QFloat::_2 * vNewRayPoint.DotProduct(vDirection);
+        const float_q &fC = vNewRayPoint.DotProduct(vNewRayPoint) - orb.Radius * orb.Radius;
+
+        // Remember that a = 1 -> D = B^2 - 4AC = B^2 - 4C
+        const float_q &fD = fB * fB - QFloat::_4 * fC;
+
+
+        if (QFloat::IsNegative(fD)) // D = B^2 - 4AC < 0 => 0 intersections
+            return EQIntersections::E_None;
+        else if (QFloat::IsZero(fD)) // D = B^2 - 4AC = 0 => 1 intersection
+        {
+            // T = -B/2A -> Remember that a=1
+            const float_q &fT = -fB*QFloat::_0_5;
+
+            // The intersection is backwards the ray origin
+            if (QFloat::IsNegative(fT))
+                return EQIntersections::E_None;
+            else
+            {
+                vPoint1 = vNewRayPoint + fT * vDirection + orb.P;
+                return EQIntersections::E_One;
+            }
+        }
+        else // D = B^2 - 4AC > 0 => 2 intersections
+        {
+            const float_q &fSqrtD = sqrt(fD);
+
+            // Closest intersection to ls.A. T1 = (-B - sqrt(D))/2A -> Remember that A = 1
+            const float_q &fT1 = -(fB + fSqrtD)*QFloat::_0_5;
+
+            // Farthest intersection to ls.A. T2 = (-B + sqrt(D))/2A -> Remember that A = 1
+            const float_q &fT2 = (-fB + fSqrtD)*QFloat::_0_5;
+
+            // Prevent rays with origin point inside orb, which must have only one intersection.
+            const bool &bT1Fails = QFloat::IsNegative(fT1);
+            const bool &bT2Fails = QFloat::IsNegative(fT2);
+
+            if (bT1Fails && bT2Fails)
+                return EQIntersections::E_None; // Shouldn't happen this :(
+            else if (bT1Fails)  // One parameter is negative, there is only one intersection
+            {
+                vPoint1 = vNewRayPoint + fT2 * vDirection + orb.P;
+                return EQIntersections::E_One;
+            }
+            else if (bT2Fails) // One parameter is negative, there is only one intersection
+            {
+                vPoint1 = vNewRayPoint + fT1 * vDirection + orb.P;
+                return EQIntersections::E_One;
+            }
+            else // Most of the cases: two intersections.
+            {
+                vPoint1 = vNewRayPoint + fT1 * vDirection + orb.P;
+                vPoint2 = vNewRayPoint + fT2 * vDirection + orb.P;
+                return EQIntersections::E_Two;
+            }
+        }
     }
 
     /// <summary>
