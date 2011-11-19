8b05f34fb3482897c7b2d9a7b5d20cc85f84dfd0
lince3d <lince3d@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-11-19 18:44:01 +0000

    143 - Crear macros para abreviar nombres de tests
    Agregadas nuevas macros para encapsular las de Boost.
    Renombrado commons por testsystem.

--- tests/unit/testmodule_tools/QVector3_Test.cpp
+++ tests/unit/testmodule_tools/QVector3_Test.cpp
@@ -4,22 +4,18 @@
 #include <boost/test/unit_test_log.hpp>
 using namespace boost::unit_test;
 
-/*#include "../common/QuimeraEngineFixtures.h"*/
+#include "../common/TestingExternalDefinitions.h"
 
 #include "QVector3.h"
+
 using Kinesis::QuimeraEngine::Tools::Math::QVector3;
-/*
-QE_TEST_SUITE_FIXTURE( QVector3_TestSuite_Fixture, "QVector3" )
 
-BOOST_FIXTURE_TEST_SUITE( QVector3_TestSuite, QVector3_TestSuite_Fixture )
-*/
-
-BOOST_AUTO_TEST_SUITE( QVector3_TestSuite )
+QTEST_SUITE_BEGIN( QVector3_TestSuite )
 
 /// <summary>
 /// Verifies whether the default constructor set all components to zero.
 /// </summary>
-BOOST_AUTO_TEST_CASE ( Constructor_0_Test )
+QTEST_CASE ( Constructor_0_Test )
 {
     // Preparation
     QVector3 target;
@@ -33,7 +29,7 @@ BOOST_AUTO_TEST_CASE ( Constructor_0_Test )
 /// <summary>
 /// Verifies whether the constructor set all components to a concrete value.
 /// </summary>
-BOOST_AUTO_TEST_CASE ( Constructor_1_Test )
+QTEST_CASE ( Constructor_1_Test )
 {
     // Preparation
     QVector3 target(QFloat::_0_5);
@@ -47,7 +43,7 @@ BOOST_AUTO_TEST_CASE ( Constructor_1_Test )
 /// <summary>
 /// Verifies that the assert fails when the vector is divided by zero.
 /// </summary>
-BOOST_AUTO_TEST_CASE ( OperatorDivision_DividedByZeroAssertionFails_Test )
+QTEST_CASE ( OperatorDivision_DividedByZeroAssertionFails_Test )
 {
     // Preparation
     QVector3 target;
@@ -65,4 +61,4 @@ BOOST_AUTO_TEST_CASE ( OperatorDivision_DividedByZeroAssertionFails_Test )
 }
 
 // End - Test Suite: QVector3
-BOOST_AUTO_TEST_SUITE_END()
+QTEST_SUITE_END()
