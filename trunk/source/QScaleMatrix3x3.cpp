951665cabe01a49770e15195136e4b0bed47d958
jwladi@gmail.com <jwladi@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2011-12-06 09:51:55 +0000

    Removes some duplicated code in QRotationMatrix3x3 and QScaleMatrix3x3 product operators

--- trunk/source/QScaleMatrix3x3.cpp
+++ trunk/source/QScaleMatrix3x3.cpp
@@ -74,80 +74,22 @@ QTransformationMatrix<QMatrix4x4> QScaleMatrix3x3::operator*(const QRotationMatr
 
 QTransformationMatrix<QMatrix4x4> QScaleMatrix3x3::operator*(const QTranslationMatrix<QMatrix4x4> &m) const
 {
-    QTransformationMatrix<QMatrix4x4> aux(QTransformationMatrix<QMatrix4x4>::Identity);
-
-    aux.ResetToIdentity();
-
-    aux.ij[0][0] = this->ij[0][0];
-    aux.ij[1][1] = this->ij[1][1];
-    aux.ij[2][2] = this->ij[2][2];
-
-    aux.ij[3][0] = m.ij[3][0];
-    aux.ij[3][1] = m.ij[3][1];
-    aux.ij[3][2] = m.ij[3][2];
-
-    return aux;
+    return this->ProductOperatorImp<QMatrix4x4>(m);
 }
 
 QTransformationMatrix<QMatrix4x3> QScaleMatrix3x3::operator*(const QTranslationMatrix<QMatrix4x3> &m) const
 {
-    QTransformationMatrix<QMatrix4x3> aux(QTransformationMatrix<QMatrix4x3>::Identity);
-
-    aux.ij[0][0] = this->ij[0][0];
-    aux.ij[1][1] = this->ij[1][1];
-    aux.ij[2][2] = this->ij[2][2];
-
-    aux.ij[3][0] = m.ij[3][0];
-    aux.ij[3][1] = m.ij[3][1];
-    aux.ij[3][2] = m.ij[3][2];
-
-    return aux;
+    return this->ProductOperatorImp<QMatrix4x3>(m);
 }
 
 QTransformationMatrix<QMatrix4x4> QScaleMatrix3x3::operator*(const QTransformationMatrix<QMatrix4x4> &m) const
 {
-    QTransformationMatrix<QMatrix4x4> aux(QTransformationMatrix<QMatrix4x4>::Identity);
-
-    aux.ij[3][0] = m.ij[3][0];
-    aux.ij[3][1] = m.ij[3][1];
-    aux.ij[3][2] = m.ij[3][2];
-
-    aux.ij[0][0] = this->ij[0][0]*m.ij[0][0];
-    aux.ij[0][1] = this->ij[0][0]*m.ij[0][1];
-    aux.ij[0][2] = this->ij[0][0]*m.ij[0][2];
-
-    aux.ij[1][0] = this->ij[1][1]*m.ij[1][0];
-    aux.ij[1][1] = this->ij[1][1]*m.ij[1][1];
-    aux.ij[1][2] = this->ij[1][1]*m.ij[1][2];
-
-    aux.ij[2][0] = this->ij[2][2]*m.ij[2][0];
-    aux.ij[2][1] = this->ij[2][2]*m.ij[2][1];
-    aux.ij[2][2] = this->ij[2][2]*m.ij[2][2];
-
-    return aux;
+    return this->ProductOperatorImp<QMatrix4x4>(m);
 }
 
 QTransformationMatrix<QMatrix4x3> QScaleMatrix3x3::operator*(const QTransformationMatrix<QMatrix4x3> &m) const
 {
-    QTransformationMatrix<QMatrix4x3> aux(QTransformationMatrix<QMatrix4x3>::Identity);
-
-    aux.ij[3][0] = m.ij[3][0];
-    aux.ij[3][1] = m.ij[3][1];
-    aux.ij[3][2] = m.ij[3][2];
-
-    aux.ij[0][0] = this->ij[0][0]*m.ij[0][0];
-    aux.ij[0][1] = this->ij[0][0]*m.ij[0][1];
-    aux.ij[0][2] = this->ij[0][0]*m.ij[0][2];
-
-    aux.ij[1][0] = this->ij[1][1]*m.ij[1][0];
-    aux.ij[1][1] = this->ij[1][1]*m.ij[1][1];
-    aux.ij[1][2] = this->ij[1][1]*m.ij[1][2];
-
-    aux.ij[2][0] = this->ij[2][2]*m.ij[2][0];
-    aux.ij[2][1] = this->ij[2][2]*m.ij[2][1];
-    aux.ij[2][2] = this->ij[2][2]*m.ij[2][2];
-
-    return aux;
+    return this->ProductOperatorImp<QMatrix4x3>(m);
 }
 
 } //namespace Math
