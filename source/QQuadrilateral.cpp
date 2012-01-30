73b3b78c58a36cd90e65849bc9744806278cb36c
jwladi <jwladi@gmail.com>
2012-01-30 19:17:54 +0000

    231 - Corregir método IsConvex the QQuadrilateral (issue #231). Los cuadriláteros cruzados pasan a considerarse cóncavos y no convexos.

--- source/QQuadrilateral.cpp
+++ source/QQuadrilateral.cpp
@@ -27,107 +27,77 @@ bool QQuadrilateral::Contains (const QBaseVector2 &vPoint) const
 {
     if (this->IsConvex()) // Its convex
     {
-        if (this->IsCrossed()) // Its crossed
-        {
-            QLineSegment2D ls1(this->A, this->D), ls2(this->B, this->C);
-            QVector2 vAux;
+        return (PointsInSameSideOfLine(vPoint, this->C, this->A, this->B) &&
+                PointsInSameSideOfLine(vPoint, this->D, this->B, this->C) &&
+                PointsInSameSideOfLine(vPoint, this->A, this->C, this->D) &&
+                PointsInSameSideOfLine(vPoint, this->B, this->D, this->A));
+    }
+    else if (this->IsCrossed()) // Its crossed
+    {
+        QLineSegment2D ls1(this->A, this->D), ls2(this->B, this->C);
+        QVector2 vAux;
 
-            if (ls1.IntersectionPoint(ls2, vAux) == EQIntersections::E_One) // Check if AD and BC edges cross themselves
-            {
-                // It checks both triangles (ABvAux and CDvAux)
-                if (( PointsInSameSideOfLine(vPoint, vAux, this->A, this->B) &&
+        if (ls1.IntersectionPoint(ls2, vAux) == EQIntersections::E_One) // Check if AD and BC edges cross themselves
+        {
+            // It checks both triangles (ABvAux and CDvAux)
+            return (( PointsInSameSideOfLine(vPoint, vAux, this->A, this->B) &&
                       PointsInSameSideOfLine(vPoint, this->A, this->B, vAux) &&
                       PointsInSameSideOfLine(vPoint, this->B, vAux, this->A) ) ||
                     ( PointsInSameSideOfLine(vPoint, this->C, vAux, this->D) &&
                       PointsInSameSideOfLine(vPoint, vAux, this->D, this->C) &&
-                      PointsInSameSideOfLine(vPoint, this->D, this->C, vAux) ))
-
-                    return true;
-                else
-                    return false;
-            }
-            else // Check if AB and CD edges cross themselves
-            {
-                ls1 = QLineSegment2D(this->A, this->B);
-                ls2 = QLineSegment2D(this->C, this->D);
-
-                if (ls1.IntersectionPoint(ls2, vAux) == EQIntersections::E_One)
-                {
-                    // It checks both triangles (ADvAux and BCvAux)
-                    if (( PointsInSameSideOfLine(vPoint, this->A, vAux, this->D) && PointsInSameSideOfLine(vPoint, vAux, this->D, this->A) &&
-                          PointsInSameSideOfLine(vPoint, this->D, this->A, vAux) ) ||
-                        ( PointsInSameSideOfLine(vPoint, this->B, this->C, vAux) && PointsInSameSideOfLine(vPoint, this->C, vAux, this->B) &&
-                          PointsInSameSideOfLine(vPoint, vAux, this->B, this->C) ))
-
-                        return true;
-                    else
-                        return false;
-                }
-                // Something is wrong! : Its crossed quadrilateral but there are no intersections between edges.
-                else
-                    QE_ASSERT(false) 
-            }
+                      PointsInSameSideOfLine(vPoint, this->D, this->C, vAux) ));
         }
-        else // Its standard convex
+        else // AB and CD edges must cross themselves
         {
-            if (PointsInSameSideOfLine(vPoint, this->C, this->A, this->B) && PointsInSameSideOfLine(vPoint, this->D, this->B, this->C) &&
-                PointsInSameSideOfLine(vPoint, this->A, this->C, this->D) && PointsInSameSideOfLine(vPoint, this->B, this->D, this->A))
-
-                return true;
-            else
-                return false;
+            // It checks both triangles (ADvAux and BCvAux)
+            return (( PointsInSameSideOfLine(vPoint, this->A, vAux, this->D) &&
+                  PointsInSameSideOfLine(vPoint, vAux, this->D, this->A) &&
+                  PointsInSameSideOfLine(vPoint, this->D, this->A, vAux) ) ||
+                ( PointsInSameSideOfLine(vPoint, this->B, this->C, vAux) &&
+                  PointsInSameSideOfLine(vPoint, this->C, vAux, this->B) &&
+                  PointsInSameSideOfLine(vPoint, vAux, this->B, this->C) ));
         }
     }
     else if (this->IsConcaveHere(this->A, this->B, this->D, this->C)) // Its concave in A vertex
     {
         // We check the two triangles around A vertex
-        if (( PointsInSameSideOfLine(vPoint, this->C, this->A, this->B) && PointsInSameSideOfLine(vPoint, this->A, this->B, this->C) &&
-              PointsInSameSideOfLine(vPoint, this->B, this->C, this->A) ) ||
-            ( PointsInSameSideOfLine(vPoint, this->C, this->A, this->D) && PointsInSameSideOfLine(vPoint, this->A, this->D, this->C) &&
-              PointsInSameSideOfLine(vPoint, this->D, this->C, this->A) ))
-
-            return true;
-        else
-            return false;
+        return (( PointsInSameSideOfLine(vPoint, this->C, this->A, this->B) &&
+                  PointsInSameSideOfLine(vPoint, this->A, this->B, this->C) &&
+                  PointsInSameSideOfLine(vPoint, this->B, this->C, this->A) ) ||
+                ( PointsInSameSideOfLine(vPoint, this->C, this->A, this->D) &&
+                  PointsInSameSideOfLine(vPoint, this->A, this->D, this->C) &&
+                  PointsInSameSideOfLine(vPoint, this->D, this->C, this->A) ));
     }
     else if (this->IsConcaveHere(this->B, this->A, this->C, this->D)) // Its concave in B vertex
     {
         // We check the two triangles around B vertex
-        if (( PointsInSameSideOfLine(vPoint, this->D, this->B, this->C) && PointsInSameSideOfLine(vPoint, this->B, this->C, this->D) &&
-              PointsInSameSideOfLine(vPoint, this->C, this->D, this->B) ) ||
-            ( PointsInSameSideOfLine(vPoint, this->A, this->B, this->D) && PointsInSameSideOfLine(vPoint, this->B, this->D, this->A) &&
-              PointsInSameSideOfLine(vPoint, this->D, this->A, this->B) ))
-
-            return true;
-        else
-            return false;
+        return (( PointsInSameSideOfLine(vPoint, this->D, this->B, this->C) &&
+                  PointsInSameSideOfLine(vPoint, this->B, this->C, this->D) &&
+                  PointsInSameSideOfLine(vPoint, this->C, this->D, this->B) ) ||
+                ( PointsInSameSideOfLine(vPoint, this->A, this->B, this->D) &&
+                  PointsInSameSideOfLine(vPoint, this->B, this->D, this->A) &&
+                  PointsInSameSideOfLine(vPoint, this->D, this->A, this->B) ));
     }
     else if (this->IsConcaveHere(this->C, this->B, this->D, this->A)) // Its concave in C vertex
     {
         // We check the two triangles around C vertex
-        if (( PointsInSameSideOfLine(vPoint, this->A, this->C, this->D) && PointsInSameSideOfLine(vPoint, this->C, this->D, this->A) &&
-              PointsInSameSideOfLine(vPoint, this->D, this->A, this->C) ) ||
-            ( PointsInSameSideOfLine(vPoint, this->B, this->C, this->A) && PointsInSameSideOfLine(vPoint, this->C, this->A, this->B) &&
-              PointsInSameSideOfLine(vPoint, this->A, this->B, this->C) ))
-
-            return true;
-        else
-            return false;
+        return (( PointsInSameSideOfLine(vPoint, this->A, this->C, this->D) &&
+                  PointsInSameSideOfLine(vPoint, this->C, this->D, this->A) &&
+                  PointsInSameSideOfLine(vPoint, this->D, this->A, this->C) ) ||
+                ( PointsInSameSideOfLine(vPoint, this->B, this->C, this->A) &&
+                  PointsInSameSideOfLine(vPoint, this->C, this->A, this->B) &&
+                  PointsInSameSideOfLine(vPoint, this->A, this->B, this->C) ));
     }
-    else if (this->IsConcaveHere(this->D, this->A, this->C, this->B)) // Its concave in D vertex
+    else // It must be concave in D vertex
     {
         // We check the two triangles around D vertex
-        if (( PointsInSameSideOfLine(vPoint, this->B, this->D, this->A) && PointsInSameSideOfLine(vPoint, this->D, this->A, this->B) &&
-              PointsInSameSideOfLine(vPoint, this->A, this->B, this->D) ) ||
-            ( PointsInSameSideOfLine(vPoint, this->C, this->D, this->B) && PointsInSameSideOfLine(vPoint, this->D, this->B, this->C) &&
-              PointsInSameSideOfLine(vPoint, this->B, this->C, this->D) ))
-
-            return true;
-        else
-            return false;
+        return (( PointsInSameSideOfLine(vPoint, this->B, this->D, this->A) &&
+                  PointsInSameSideOfLine(vPoint, this->D, this->A, this->B) &&
+                  PointsInSameSideOfLine(vPoint, this->A, this->B, this->D) ) ||
+                ( PointsInSameSideOfLine(vPoint, this->C, this->D, this->B) &&
+                  PointsInSameSideOfLine(vPoint, this->D, this->B, this->C) &&
+                  PointsInSameSideOfLine(vPoint, this->B, this->C, this->D) ));
     }
-    else // Something is wrong! : its concave but no vertex has concavity
-        QE_ASSERT(false) 
 }
 
 bool QQuadrilateral::Intersection(const QBaseQuadrilateral &quad) const
@@ -159,13 +129,10 @@ bool QQuadrilateral::Intersection(const QBaseQuadrilateral &quad) const
 
 bool QQuadrilateral::IsCrossed() const
 {
-    if ( ( !PointsInSameSideOfLine(this->A, this->D, this->B, this->C) &&
-            !PointsInSameSideOfLine(this->C, this->B, this->A, this->D) ) ||
-            ( !PointsInSameSideOfLine(this->A, this->B, this->D, this->C) &&
-            !PointsInSameSideOfLine(this->C, this->D, this->A, this->B) ) )
-        return true;
-    else
-        return false;
+    return ( ( !PointsInSameSideOfLine(this->A, this->D, this->B, this->C) &&
+               !PointsInSameSideOfLine(this->C, this->B, this->A, this->D) ) ||
+             ( !PointsInSameSideOfLine(this->A, this->B, this->D, this->C) &&
+               !PointsInSameSideOfLine(this->C, this->D, this->A, this->B) ) );
 }
 
 bool QQuadrilateral::IsConvex() const
@@ -173,10 +140,7 @@ bool QQuadrilateral::IsConvex() const
     QLineSegment2D ls1(this->A, this->C);
     QLineSegment2D ls2(this->B, this->D);
 
-    if (ls1.Intersection(ls2))
-        return true;
-    else
-        return this->IsCrossed(); // Crossed quadrilateral diagonals don't intersects.
+    return ls1.Intersection(ls2);
 }
 
 bool QQuadrilateral::IsConcave() const
@@ -265,17 +229,15 @@ bool QQuadrilateral::PointsInSameSideOfLine(const QBaseVector2 &vPoint1, const Q
 
     if ( SQFloat::IsZero(fOrientation1) || SQFloat::IsZero(fOrientation2) )
         return true;
-    else if ( SQFloat::IsNegative(fOrientation1) == SQFloat::IsNegative(fOrientation2) )
-        return true;
     else
-        return false;
+        return ( SQFloat::IsNegative(fOrientation1) == SQFloat::IsNegative(fOrientation2) );
 }
 
 
 bool QQuadrilateral::IsConcaveHere(const QVector2 &vAngleVertex, const QVector2 &vAngleEndVertex1,
                                    const QVector2 &vAngleEndVertex2, const QVector2 &vOppositeVertex) const
 {
-    if (this->IsConvex())
+    if (this->IsConvex() || this->IsCrossed()) // There isn't a concave angle.
         return false;
     else if ( !PointsInSameSideOfLine(vOppositeVertex, vAngleVertex, vAngleEndVertex1, vAngleEndVertex2) )
         return false; // The outer diagonal should not divide quadrilateral
@@ -284,10 +246,7 @@ bool QQuadrilateral::IsConcaveHere(const QVector2 &vAngleVertex, const QVector2 
         QLineSegment2D ls(vAngleEndVertex1, vAngleEndVertex2);
 
         // Angle vertex must be the closest to outer diagonal
-        if ( ls.MinDistance(vAngleVertex) < ls.MinDistance(vOppositeVertex) )
-            return true;
-        else
-            return false;
+        return ( ls.MinDistance(vAngleVertex) < ls.MinDistance(vOppositeVertex) );
     }
 }
 
