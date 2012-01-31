3a160aa24c7ce684b48e9ba148cbe3e38711d864
javiermartingarcia@gmail.com <javiermartingarcia@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2012-01-31 22:29:49 +0000

    235 - Unit tests: QBaseMatrix2x2, QBaseMatrix3x3, QBaseMatrix4x3, QBaseMatrix3x4, QBaseMatrix4x4
    Se añaden tests unitarios basicos para QBaseMatrix2x2, QBaseMatrix3x3, QBaseMatrix4x3, QBaseMatrix3x4, QBaseMatrix4x4.
    Incluye la actualizacion de los proyectos de VS y Code::Blocks.

---
+++ trunk/testing/tests/unit/testmodule_tools/QBaseMatrix2x2_Test.cpp
@@ -0,0 +1,262 @@
+// [TERMS&CONDITIONS]
+
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+using namespace boost::unit_test;
+
+#include "../../testsystem/TestingExternalDefinitions.h"
+
+#include "QBaseMatrix2x2.h"
+
+using Kinesis::QuimeraEngine::Tools::Math::QBaseMatrix2x2;
+
+QTEST_SUITE_BEGIN( QBaseMatrix2x2_TestSuite )
+
+/// <summary>
+/// Checks if default values have changed.
+/// </summary>
+QTEST_CASE ( Constructor1_DefaultValuesHaventChanged_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_ALL = SQFloat::_0;
+
+    QBaseMatrix2x2 matrixUT;
+
+    // Verification
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][0], EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][1], EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][0], EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][1], EXPECTED_VALUE_FOR_ALL);
+}
+
+/// <summary>
+/// Checks if the value provided is set to all the matrix components.
+/// </summary>
+QTEST_CASE ( Constructor2_ValueIsSetForAllComponents_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_ALL = SQFloat::_5;
+
+    QBaseMatrix2x2 matrixUT(EXPECTED_VALUE_FOR_ALL);
+
+    // Verification
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][0], EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][1], EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][0], EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][1], EXPECTED_VALUE_FOR_ALL);
+}
+
+/// <summary>
+/// Checks if values used as parameters are properly set to matrix components.
+/// </summary>
+QTEST_CASE ( Constructor3_ValuesAreSetProperly_Test )
+{
+   // Preparation
+    const float_q EXPECTED_VALUE_FOR_00 = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_01 = SQFloat::_2;
+    const float_q EXPECTED_VALUE_FOR_10 = SQFloat::_3;
+    const float_q EXPECTED_VALUE_FOR_11 = SQFloat::_4;
+
+	// Execution
+    QBaseMatrix2x2 matrixUT(EXPECTED_VALUE_FOR_00,EXPECTED_VALUE_FOR_01,EXPECTED_VALUE_FOR_10,EXPECTED_VALUE_FOR_11);
+
+    // Verification
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][0], EXPECTED_VALUE_FOR_00);
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][1], EXPECTED_VALUE_FOR_01);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][0], EXPECTED_VALUE_FOR_10);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][1], EXPECTED_VALUE_FOR_11);
+}
+
+/// <summary>
+/// Checks if the matrix components are set to the correct floats stored in a valid memory space.
+/// </summary>
+QTEST_CASE ( Constructor4_MatrixComponentsAreFilledWithValidReferenceTo4Floats_Test )
+{
+   // Preparation
+    const float_q EXPECTED_VALUE_FOR_00 = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_01 = SQFloat::_2;
+    const float_q EXPECTED_VALUE_FOR_10 = SQFloat::_3;
+    const float_q EXPECTED_VALUE_FOR_11 = SQFloat::_4;
+
+    float_q* VALID_ARRAY_OF_4_FLOATS = new float_q[4];
+    VALID_ARRAY_OF_4_FLOATS[0] = EXPECTED_VALUE_FOR_00;
+    VALID_ARRAY_OF_4_FLOATS[1] = EXPECTED_VALUE_FOR_01;
+    VALID_ARRAY_OF_4_FLOATS[2] = EXPECTED_VALUE_FOR_10;
+    VALID_ARRAY_OF_4_FLOATS[3] = EXPECTED_VALUE_FOR_11;
+
+	// Execution
+    QBaseMatrix2x2 matrixUT(VALID_ARRAY_OF_4_FLOATS);
+
+    // Verification
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][0], EXPECTED_VALUE_FOR_00);
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][1], EXPECTED_VALUE_FOR_01);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][0], EXPECTED_VALUE_FOR_10);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][1], EXPECTED_VALUE_FOR_11);
+
+    // Cleanup
+    delete [] VALID_ARRAY_OF_4_FLOATS;
+    VALID_ARRAY_OF_4_FLOATS = null_q;
+}
+
+/// <summary>
+/// Checks if the assertion inside the function fails when a null pointer is received.
+/// </summary>
+QTEST_CASE ( Constructor4_AssertionFailsWhenPointerIsNull_Test )
+{
+    // Preparation
+    const float_q* NULL_ARRAY = null_q;
+
+	// Execution
+    bool bAssertionFailed = false;
+
+    try
+    {
+        QBaseMatrix2x2 vMatrixUT(NULL_ARRAY);
+    }
+    catch(...) // TODO [jmartin]: Only must catch the proper exception class, not implemented yet
+    {
+        bAssertionFailed = true;
+    }
+
+    // Verification
+    BOOST_CHECK(bAssertionFailed);
+}
+
+/// <summary>
+/// Checks if matrix components are set to the correct values packed in a valid vf32 object.
+/// </summary>
+QTEST_CASE ( Constructor5_MatrixComponentsAreSetToValidVF32PackedValues_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_00 = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_01 = SQFloat::_2;
+    const float_q EXPECTED_VALUE_FOR_10 = SQFloat::_3;
+    const float_q EXPECTED_VALUE_FOR_11 = SQFloat::_4;
+
+	vf32_q PACK;
+	SQVF32::Pack(EXPECTED_VALUE_FOR_00, EXPECTED_VALUE_FOR_01, EXPECTED_VALUE_FOR_10, EXPECTED_VALUE_FOR_11, PACK);
+
+	// Execution
+	QBaseMatrix2x2 matrixUT(PACK);
+
+    // Verification
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][0], EXPECTED_VALUE_FOR_00);
+    BOOST_CHECK_EQUAL(matrixUT.ij[0][1], EXPECTED_VALUE_FOR_01);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][0], EXPECTED_VALUE_FOR_10);
+    BOOST_CHECK_EQUAL(matrixUT.ij[1][1], EXPECTED_VALUE_FOR_11);
+}
+
+/// <summary>
+/// Checks if the equality operator returns true when operand components differences equals tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferTolerance_Test )
+{
+    // Preparation
+    const QBaseMatrix2x2 LEFT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the equality operator returns true when operand components differences are lower than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferLessThanTolerance_Test )
+{
+    // Preparation
+    const float_q HALF_TOLERANCE = SQFloat::Epsilon * SQFloat::_0_5;
+
+    const QBaseMatrix2x2 LEFT_OPERAND(HALF_TOLERANCE, HALF_TOLERANCE, HALF_TOLERANCE, HALF_TOLERANCE);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the equality operator returns false when operand components differences are greater than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_FalseWhenOperandsDifferGreaterThanTolerance_Test )
+{
+    // Preparation
+    const float_q ONE_AND_A_HALF_TOLERANCE = SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5;
+
+    const QBaseMatrix2x2 LEFT_OPERAND(ONE_AND_A_HALF_TOLERANCE, ONE_AND_A_HALF_TOLERANCE, ONE_AND_A_HALF_TOLERANCE, ONE_AND_A_HALF_TOLERANCE);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND == RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the equality operator returns true when operand components are exactly equal.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsAreExactlyEqual_Test )
+{
+    // Preparation
+    const QBaseMatrix2x2 LEFT_OPERAND(SQFloat::_1, SQFloat::_2, SQFloat::_3, SQFloat::_4);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_1, SQFloat::_2, SQFloat::_3, SQFloat::_4);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the inequality operator returns false when operand components differences equals tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferTolerance_Test )
+{
+    // Preparation
+    const QBaseMatrix2x2 LEFT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the inequality operator returns false when operand components differences are lower than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferLessThanTolerance_Test )
+{
+    const float_q HALF_TOLERANCE = SQFloat::Epsilon * SQFloat::_0_5;
+
+   // Preparation
+    const QBaseMatrix2x2 LEFT_OPERAND(HALF_TOLERANCE, HALF_TOLERANCE, HALF_TOLERANCE, HALF_TOLERANCE);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the inequality operator returns true when operand components differences are greater than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_TrueWhenOperandsDifferGreaterThanTolerance_Test )
+{
+    // Preparation
+    const float_q ONE_AND_A_HALF_TOLERANCE = SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5;
+
+    const QBaseMatrix2x2 LEFT_OPERAND(ONE_AND_A_HALF_TOLERANCE, ONE_AND_A_HALF_TOLERANCE, ONE_AND_A_HALF_TOLERANCE, ONE_AND_A_HALF_TOLERANCE);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND != RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the inequality operator returns false when operand components are exactly equal.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsAreExactlyEqual_Test )
+{
+    // Preparation
+    const QBaseMatrix2x2 LEFT_OPERAND(SQFloat::_1, SQFloat::_2, SQFloat::_3, SQFloat::_4);
+    const QBaseMatrix2x2 RIGHT_OPERAND(SQFloat::_1, SQFloat::_2, SQFloat::_3, SQFloat::_4);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND));
+}
+
+// End - Test Suite: QBaseMatrix2x2
+QTEST_SUITE_END()
