f63ee9f29127810359be8bfe76e6885142216eb1
lince3d <lince3d@7af85c2e-922d-2424-809b-042d02d0a1c2>
2012-02-23 20:24:54 +0000

    Unit tests para QVector3 creados. Fallan algunos, hay que encontrar dónde está el problema cuando se testeen las clases implicadas.

---
+++ trunk/testing/tests/unit/testmodule_tools/QVector3WhiteBox.h
@@ -0,0 +1,72 @@
+// [TERMS&CONDITIONS]
+
+#ifndef __QVECTOR3WHITEBOX__
+#define __QVECTOR3WHITEBOX__
+
+#include "QVector3.h"
+
+using Kinesis::QuimeraEngine::Tools::Math::QVector3;
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace Tools
+{
+namespace Math
+{
+namespace Test
+{
+
+/// <summary>
+/// Class intented to be used to expose protected methods of QVector3 for testing purposes.
+/// </summary>
+class QDllExport QVector3WhiteBox : public QVector3
+{
+	// CONSTRUCTORS
+	// ---------------
+public:
+
+	/// <summary>
+	/// Default constructor.
+	/// </summary>
+	QVector3WhiteBox();
+
+    // Necessary for testing
+    QVector3WhiteBox(const QVector3& vVector) : QVector3(vVector)
+    {
+    }
+
+
+	// METHODS
+	// ---------------
+public:
+
+    // Necessary for testing
+    QVector3WhiteBox& operator=(const QVector3 &vVector)
+    {
+        QVector3::operator=(vVector);
+    }
+
+    // Exposed method
+    template <class MatrixType>
+	void TransformImp(const QTranslationMatrix<MatrixType> &translation)
+    {
+        QVector3::TransformImp(translation);
+    }
+
+    // Exposed method
+    template <class MatrixType>
+	void TransformImp(const QTransformationMatrix<MatrixType> &transformation)
+    {
+        QVector3::TransformImp(transformation);
+    }
+};
+
+} //namespace Test
+} //namespace Math
+} //namespace Tools
+} //namespace QuimeraEngine
+} //namespace Kinesis
+
+#endif // __QVECTOR3WHITEBOX__
