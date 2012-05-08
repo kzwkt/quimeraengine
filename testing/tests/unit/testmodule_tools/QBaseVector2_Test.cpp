3e9cf7f6db860874b751ba30ddfd0b294c1938e4
jwladi <jwladi@gmail.com>
2012-05-08 21:42:12 +0000

    233 - Unit tests: QBaseVector4, QBaseVector2, QVector2

---
+++ testing/tests/unit/testmodule_tools/QBaseVector2_Test.cpp
@@ -0,0 +1,243 @@
+// [TERMS&CONDITIONS]
+
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+using namespace boost::unit_test;
+
+#include "../../testsystem/TestingExternalDefinitions.h"
+
+#include "QBaseVector2.h"
+#include "SQVF32.h"
+
+using Kinesis::QuimeraEngine::Tools::Math::QBaseVector2;
+
+QTEST_SUITE_BEGIN( QBaseVector2_TestSuite )
+
+/// <summary>
+/// Checks if default values has changed.
+/// </summary>
+QTEST_CASE ( Constructor1_DefaultValuesHasntChanged_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_0;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_0;
+
+	// Execution
+    QBaseVector2 vVectorUT;
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_X);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_Y);
+}
+
+/// <summary>
+/// Checks if values used as parameters are properly set to vector components.
+/// </summary>
+QTEST_CASE ( Constructor2_ValuesAreSetProperly_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
+
+	// Execution
+    QBaseVector2 vVectorUT(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_X);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_Y);
+}
+
+/// <summary>
+/// Checks if the value provided is set to all the vector components.
+/// </summary>
+QTEST_CASE ( Constructor3_ValueIsSetForAllComponents_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_ALL = SQFloat::_5;
+
+	// Execution
+    QBaseVector2 vVectorUT(EXPECTED_VALUE_FOR_ALL);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_ALL);
+}
+
+/// <summary>
+/// Checks if the vector components are set to the correct floats stored in a valid memory space.
+/// </summary>
+QTEST_CASE ( Constructor4_VectorComponentsAreFilledWithValidReferenceTo2Floats_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
+
+    float_q* VALID_ARRAY_OF_2_FLOATS = new float_q[2];
+
+    VALID_ARRAY_OF_2_FLOATS[0] = EXPECTED_VALUE_FOR_X;
+    VALID_ARRAY_OF_2_FLOATS[1] = EXPECTED_VALUE_FOR_Y;
+
+	// Execution
+    QBaseVector2 vVectorUT(VALID_ARRAY_OF_2_FLOATS);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, VALID_ARRAY_OF_2_FLOATS[0]);
+    BOOST_CHECK_EQUAL(vVectorUT.y, VALID_ARRAY_OF_2_FLOATS[1]);
+
+    // Cleaning
+    delete[] VALID_ARRAY_OF_2_FLOATS;
+}
+
+/// <summary>
+/// Checks if the assertion inside the function fails when a null pointer is received.
+/// </summary>
+QTEST_CASE ( Constructor4_AssertionFailsWhenPointerIsNull_Test )
+{
+    // Preparation
+    const float_q* NULL_ARRAY = null_q;
+
+	// Execution
+    bool bAssertionFailed = false;
+
+    try
+    {
+        QBaseVector2 vVectorUT(NULL_ARRAY);
+    }
+    catch(...) // TODO [Thund]: Only must catch the proper exception class, not implemented yet
+    {
+        bAssertionFailed = true;
+    }
+
+    // Verification
+    BOOST_CHECK(bAssertionFailed);
+}
+
+/// <summary>
+/// Checks if vector components are set to the correct values packed in a valid vf32 object.
+/// </summary>
+QTEST_CASE ( Constructor5_VectorComponentsAreSetToValidVF32PackedValues_Test )
+{
+    // Preparation
+    using Kinesis::QuimeraEngine::Tools::DataTypes::SQVF32;
+    using Kinesis::QuimeraEngine::Tools::DataTypes::vf32_q;
+
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
+
+	vf32_q PACK;
+	SQVF32::Pack(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y, SQFloat::_0, SQFloat::_0, PACK);
+
+	// Execution
+	QBaseVector2 vVectorUT(PACK);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_X);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_Y);
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components differences equals tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferTolerance_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components differences are lower than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferLessThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon - SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences are greater than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_FalseWhenOperandsDifferGreaterThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND == RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components are exactly equal.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsAreExactlyEqual_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::Epsilon);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences equals tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferTolerance_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences are lower than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferLessThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon - SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences are greater than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_TrueWhenOperandsDifferGreaterThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND != RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components are exactly equal.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsAreExactlyEqual_Test )
+{
+    // Preparation
+    const QBaseVector2 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector2 RIGHT_OPERAND(SQFloat::Epsilon);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+// End - Test Suite: QBaseVector2
+QTEST_SUITE_END()
---
+++ testing/tests/unit/testmodule_tools/QBaseVector4_Test.cpp
@@ -0,0 +1,262 @@
+// [TERMS&CONDITIONS]
+
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+using namespace boost::unit_test;
+
+#include "../../testsystem/TestingExternalDefinitions.h"
+
+#include "QBaseVector4.h"
+#include "SQVF32.h"
+
+using Kinesis::QuimeraEngine::Tools::Math::QBaseVector4;
+
+QTEST_SUITE_BEGIN( QBaseVector4_TestSuite )
+
+/// <summary>
+/// Checks if default values has changed.
+/// </summary>
+QTEST_CASE ( Constructor1_DefaultValuesHasntChanged_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_0;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_0;
+    const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_0;
+    const float_q EXPECTED_VALUE_FOR_W = SQFloat::_0;
+
+	// Execution
+    QBaseVector4 vVectorUT;
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_X);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_Y);
+    BOOST_CHECK_EQUAL(vVectorUT.z, EXPECTED_VALUE_FOR_Z);
+    BOOST_CHECK_EQUAL(vVectorUT.w, EXPECTED_VALUE_FOR_W);
+}
+
+/// <summary>
+/// Checks if values used as parameters are properly set to vector components.
+/// </summary>
+QTEST_CASE ( Constructor2_ValuesAreSetProperly_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
+    const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_3;
+    const float_q EXPECTED_VALUE_FOR_W = SQFloat::_4;
+
+	// Execution
+    QBaseVector4 vVectorUT(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y, EXPECTED_VALUE_FOR_Z, EXPECTED_VALUE_FOR_W);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_X);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_Y);
+    BOOST_CHECK_EQUAL(vVectorUT.z, EXPECTED_VALUE_FOR_Z);
+    BOOST_CHECK_EQUAL(vVectorUT.w, EXPECTED_VALUE_FOR_W);
+}
+
+/// <summary>
+/// Checks if the value provided is set to all the vector components.
+/// </summary>
+QTEST_CASE ( Constructor3_ValueIsSetForAllComponents_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_ALL = SQFloat::_5;
+
+	// Execution
+    QBaseVector4 vVectorUT(EXPECTED_VALUE_FOR_ALL);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(vVectorUT.z, EXPECTED_VALUE_FOR_ALL);
+    BOOST_CHECK_EQUAL(vVectorUT.w, EXPECTED_VALUE_FOR_ALL);
+}
+
+/// <summary>
+/// Checks if the vector components are set to the correct floats stored in a valid memory space.
+/// </summary>
+QTEST_CASE ( Constructor4_VectorComponentsAreFilledWithValidReferenceTo3Floats_Test )
+{
+    // Preparation
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
+    const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_3;
+    const float_q EXPECTED_VALUE_FOR_W = SQFloat::_4;
+
+    float_q* VALID_ARRAY_OF_4_FLOATS = new float_q[4];
+    VALID_ARRAY_OF_4_FLOATS[0] = EXPECTED_VALUE_FOR_X;
+    VALID_ARRAY_OF_4_FLOATS[1] = EXPECTED_VALUE_FOR_Y;
+    VALID_ARRAY_OF_4_FLOATS[2] = EXPECTED_VALUE_FOR_Z;
+    VALID_ARRAY_OF_4_FLOATS[3] = EXPECTED_VALUE_FOR_W;
+
+	// Execution
+    QBaseVector4 vVectorUT(VALID_ARRAY_OF_4_FLOATS);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, VALID_ARRAY_OF_4_FLOATS[0]);
+    BOOST_CHECK_EQUAL(vVectorUT.y, VALID_ARRAY_OF_4_FLOATS[1]);
+    BOOST_CHECK_EQUAL(vVectorUT.z, VALID_ARRAY_OF_4_FLOATS[2]);
+    BOOST_CHECK_EQUAL(vVectorUT.w, VALID_ARRAY_OF_4_FLOATS[3]);
+
+    // Cleaning
+    delete[] VALID_ARRAY_OF_4_FLOATS;
+}
+
+/// <summary>
+/// Checks if the assertion inside the function fails when a null pointer is received.
+/// </summary>
+QTEST_CASE ( Constructor4_AssertionFailsWhenPointerIsNull_Test )
+{
+    // Preparation
+    const float_q* NULL_ARRAY = null_q;
+
+	// Execution
+    bool bAssertionFailed = false;
+
+    try
+    {
+        QBaseVector4 vVectorUT(NULL_ARRAY);
+    }
+    catch(...) // TODO [Thund]: Only must catch the proper exception class, not implemented yet
+    {
+        bAssertionFailed = true;
+    }
+
+    // Verification
+    BOOST_CHECK(bAssertionFailed);
+}
+
+/// <summary>
+/// Checks if vector components are set to the correct values packed in a valid vf32 object.
+/// </summary>
+QTEST_CASE ( Constructor5_VectorComponentsAreSetToValidVF32PackedValues_Test )
+{
+    // Preparation
+    using Kinesis::QuimeraEngine::Tools::DataTypes::SQVF32;
+    using Kinesis::QuimeraEngine::Tools::DataTypes::vf32_q;
+
+    const float_q EXPECTED_VALUE_FOR_X = SQFloat::_1;
+    const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
+    const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_3;
+    const float_q EXPECTED_VALUE_FOR_W = SQFloat::_4;
+
+	vf32_q PACK;
+	SQVF32::Pack(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y, EXPECTED_VALUE_FOR_Z, EXPECTED_VALUE_FOR_W, PACK);
+
+	// Execution
+	QBaseVector4 vVectorUT(PACK);
+
+    // Verification
+    BOOST_CHECK_EQUAL(vVectorUT.x, EXPECTED_VALUE_FOR_X);
+    BOOST_CHECK_EQUAL(vVectorUT.y, EXPECTED_VALUE_FOR_Y);
+    BOOST_CHECK_EQUAL(vVectorUT.z, EXPECTED_VALUE_FOR_Z);
+    BOOST_CHECK_EQUAL(vVectorUT.w, EXPECTED_VALUE_FOR_W);
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components differences equals tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferTolerance_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components differences are lower than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferLessThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon - SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences are greater than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorEquality_FalseWhenOperandsDifferGreaterThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND == RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components are exactly equal.
+/// </summary>
+QTEST_CASE ( OperatorEquality_TrueWhenOperandsAreExactlyEqual_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::Epsilon);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences equals tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferTolerance_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences are lower than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferLessThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon - SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+/// <summary>
+/// Checks if the operator returns false when operand components differences are greater than tolerance value.
+/// </summary>
+QTEST_CASE ( OperatorInequality_TrueWhenOperandsDifferGreaterThanTolerance_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::_0);
+
+	// Execution / Verification
+    BOOST_CHECK(LEFT_OPERAND != RIGHT_OPERAND);
+}
+
+/// <summary>
+/// Checks if the operator returns true when operand components are exactly equal.
+/// </summary>
+QTEST_CASE ( OperatorInequality_FalseWhenOperandsAreExactlyEqual_Test )
+{
+    // Preparation
+    const QBaseVector4 LEFT_OPERAND(SQFloat::Epsilon);
+    const QBaseVector4 RIGHT_OPERAND(SQFloat::Epsilon);
+
+	// Execution / Verification
+    BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
+}
+
+// End - Test Suite: QBaseVector4
+QTEST_SUITE_END()
