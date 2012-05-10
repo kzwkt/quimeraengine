946b29d539079a0c8d91dccecf5b0b3d2d44cf2d
jwladi@gmail.com <jwladi@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2012-05-10 11:14:06 +0000

    244 - Crear POC con wxWidgets Código de la prueba de concepto para facilitar la comprensión del desarrollo con wxWidgets, a través de wxFormBuilder.

---
+++ team/poc/Task 244 - POCFormBuilder/POCFormBuilder/POCFormBuilderApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      POCFormBuilderApp.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2012-05-09
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef POCFORMBUILDERAPP_H
+#define POCFORMBUILDERAPP_H
+
+#include <wx/app.h>
+
+class POCFormBuilderApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // POCFORMBUILDERAPP_H
