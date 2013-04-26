9c9c2c4773d222b2171ae026f73378b6aefcdb66
david.luis.87 <david.luis.87@gmail.com>
2013-04-26 19:46:19 +0000

    357 - Unit tests: QBaseQuadrilateral Unit test for class QBaseQuadrilateral

--- testing/tests/unit/testmodule_tools/QBaseQuaternion_Test.cpp
+++ testing/tests/unit/testmodule_tools/QBaseQuaternion_Test.cpp
@@ -46,10 +46,10 @@ QTEST_CASE ( Constructor1_DefaultValuesHasNotChanged_Test )
     const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_0;
     const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_0;
     const float_q EXPECTED_VALUE_FOR_W = SQFloat::_0;
-
-	// Execution
+    
+    // Execution
     QBaseQuaternion qQuaternionUT;
-
+    
     // Verification
     BOOST_CHECK_EQUAL(qQuaternionUT.x, EXPECTED_VALUE_FOR_X);
     BOOST_CHECK_EQUAL(qQuaternionUT.y, EXPECTED_VALUE_FOR_Y);
@@ -67,12 +67,12 @@ QTEST_CASE ( Constructor2_ValuesAreCopiedProperly_Test )
     const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
     const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_3;
     const float_q EXPECTED_VALUE_FOR_W = SQFloat::_4;
-
+    
     const QBaseQuaternion QUAT_TO_COPY(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y, EXPECTED_VALUE_FOR_Z, EXPECTED_VALUE_FOR_W);
-
-	// Execution
+    
+    // Execution
     QBaseQuaternion qQuatUT = QUAT_TO_COPY;
-
+    
     // Verification
     BOOST_CHECK_EQUAL(qQuatUT.x, EXPECTED_VALUE_FOR_X);
     BOOST_CHECK_EQUAL(qQuatUT.y, EXPECTED_VALUE_FOR_Y);
@@ -90,10 +90,10 @@ QTEST_CASE ( Constructor3_ValuesAreCorrectlySet_Test )
     const float_q EXPECTED_VALUE_Y = SQFloat::_2;
     const float_q EXPECTED_VALUE_Z = SQFloat::_3;
     const float_q EXPECTED_VALUE_W = SQFloat::_4;
-
-	// Execution
+    
+    // Execution
     QBaseQuaternion qQuaternionUT(SQFloat::_1, SQFloat::_2, SQFloat::_3, SQFloat::_4);
-
+    
     // Verification
     BOOST_CHECK_EQUAL(qQuaternionUT.x, EXPECTED_VALUE_X);
     BOOST_CHECK_EQUAL(qQuaternionUT.y, EXPECTED_VALUE_Y);
@@ -111,22 +111,22 @@ QTEST_CASE ( Constructor4_QuaternionComponentsAreFilledWithValidReferenceTo4Floa
     const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
     const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_3;
     const float_q EXPECTED_VALUE_FOR_W = SQFloat::_4;
-
+    
     float_q* VALID_ARRAY_OF_4_FLOATS = new float_q[4];
     VALID_ARRAY_OF_4_FLOATS[0] = EXPECTED_VALUE_FOR_X;
     VALID_ARRAY_OF_4_FLOATS[1] = EXPECTED_VALUE_FOR_Y;
     VALID_ARRAY_OF_4_FLOATS[2] = EXPECTED_VALUE_FOR_Z;
     VALID_ARRAY_OF_4_FLOATS[3] = EXPECTED_VALUE_FOR_W;
-
-	// Execution
+    
+    // Execution
     QBaseQuaternion qQuaternionUT(VALID_ARRAY_OF_4_FLOATS);
-
+    
     // Verification
     BOOST_CHECK_EQUAL(qQuaternionUT.x, VALID_ARRAY_OF_4_FLOATS[0]);
     BOOST_CHECK_EQUAL(qQuaternionUT.y, VALID_ARRAY_OF_4_FLOATS[1]);
     BOOST_CHECK_EQUAL(qQuaternionUT.z, VALID_ARRAY_OF_4_FLOATS[2]);
     BOOST_CHECK_EQUAL(qQuaternionUT.w, VALID_ARRAY_OF_4_FLOATS[3]);
-
+    
     // Cleaning
     delete[] VALID_ARRAY_OF_4_FLOATS;
 }
@@ -138,10 +138,10 @@ QTEST_CASE ( Constructor4_AssertionFailsWhenPointerIsNull_Test )
 {
     // Preparation
     const float_q* NULL_ARRAY = null_q;
-
-	// Execution
+    
+    // Execution
     bool bAssertionFailed = false;
-
+    
     try
     {
         QBaseQuaternion qQuaternionUT(NULL_ARRAY);
@@ -150,7 +150,7 @@ QTEST_CASE ( Constructor4_AssertionFailsWhenPointerIsNull_Test )
     {
         bAssertionFailed = true;
     }
-
+    
     // Verification
     BOOST_CHECK(bAssertionFailed);
 }
@@ -165,13 +165,13 @@ QTEST_CASE ( Constructor5_QuaternionComponentsAreSetToValidVF32PackedValues_Test
     const float_q EXPECTED_VALUE_FOR_Y = SQFloat::_2;
     const float_q EXPECTED_VALUE_FOR_Z = SQFloat::_3;
     const float_q EXPECTED_VALUE_FOR_W = SQFloat::_4;
-
-	vf32_q PACK;
-	SQVF32::Pack(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y, EXPECTED_VALUE_FOR_Z, EXPECTED_VALUE_FOR_W, PACK);
-
-	// Execution
-	QBaseQuaternion qQuaternionUT(PACK);
-
+    
+    vf32_q PACK;
+    SQVF32::Pack(EXPECTED_VALUE_FOR_X, EXPECTED_VALUE_FOR_Y, EXPECTED_VALUE_FOR_Z, EXPECTED_VALUE_FOR_W, PACK);
+    
+    // Execution
+    QBaseQuaternion qQuaternionUT(PACK);
+    
     // Verification
     BOOST_CHECK_EQUAL(qQuaternionUT.x, EXPECTED_VALUE_FOR_X);
     BOOST_CHECK_EQUAL(qQuaternionUT.y, EXPECTED_VALUE_FOR_Y);
@@ -187,8 +187,8 @@ QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferTolerance_Test )
     // Preparation
     const QBaseQuaternion LEFT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0,SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
 }
 
@@ -201,8 +201,8 @@ QTEST_CASE ( OperatorEquality_TrueWhenOperandsDifferLessThanTolerance_Test )
     const float_q VALUE_FOR_LEFT_OPERAND_COMPONENTS = SQFloat::Epsilon - SQFloat::Epsilon * SQFloat::_0_5;
     const QBaseQuaternion LEFT_OPERAND(VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
 }
 
@@ -215,8 +215,8 @@ QTEST_CASE ( OperatorEquality_FalseWhenOperandsDifferGreaterThanTolerance_Test )
     const float_q VALUE_FOR_LEFT_OPERAND_COMPONENTS = SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5;
     const QBaseQuaternion LEFT_OPERAND(VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(!( LEFT_OPERAND == RIGHT_OPERAND ));
 }
 
@@ -228,8 +228,8 @@ QTEST_CASE ( OperatorEquality_TrueWhenOperandsAreExactlyEqual_Test )
     // Preparation
     const QBaseQuaternion LEFT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(LEFT_OPERAND == RIGHT_OPERAND);
 }
 
@@ -241,8 +241,8 @@ QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferTolerance_Test )
     // Preparation
     const QBaseQuaternion LEFT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
 }
 
@@ -255,8 +255,8 @@ QTEST_CASE ( OperatorInequality_FalseWhenOperandsDifferLessThanTolerance_Test )
     const float_q VALUE_FOR_LEFT_OPERAND_COMPONENTS = SQFloat::Epsilon - SQFloat::Epsilon * SQFloat::_0_5;
     const QBaseQuaternion LEFT_OPERAND(VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
 }
 
@@ -269,8 +269,8 @@ QTEST_CASE ( OperatorInequality_TrueWhenOperandsDifferGreaterThanTolerance_Test 
     const float_q VALUE_FOR_LEFT_OPERAND_COMPONENTS = SQFloat::Epsilon + SQFloat::Epsilon * SQFloat::_0_5;
     const QBaseQuaternion LEFT_OPERAND(VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS, VALUE_FOR_LEFT_OPERAND_COMPONENTS);
     const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    
+    // Execution / Verification
     BOOST_CHECK(LEFT_OPERAND != RIGHT_OPERAND);
 }
 
@@ -281,9 +281,9 @@ QTEST_CASE ( OperatorInequality_FalseWhenOperandsAreExactlyEqual_Test )
 {
     // Preparation
     const QBaseQuaternion LEFT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
-    const QBaseQuaternion RIGHT_OPERAND(SQFloat::_0, SQFloat::_0, SQFloat::_0, SQFloat::_0);
-
-	// Execution / Verification
+    const QBaseQuaternion RIGHT_OPERAND(SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon, SQFloat::Epsilon);
+    
+    // Execution / Verification
     BOOST_CHECK(!( LEFT_OPERAND != RIGHT_OPERAND ));
 }
 
