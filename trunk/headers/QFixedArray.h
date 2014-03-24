3c61612a024405510cdb135326abf06477f1f2d2
rdelasheras@gmail.com <rdelasheras@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2014-03-24 13:49:43 +0000

    464 - QFixedArray Implementation of QFixedArray class.

---
+++ trunk/headers/QFixedArray.h
@@ -0,0 +1,289 @@
+//-------------------------------------------------------------------------------//
+//                         QUIMERA ENGINE : LICENSE                              //
+//-------------------------------------------------------------------------------//
+// This file is part of Quimera Engine.                                          //
+// Quimera Engine is free software: you can redistribute it and/or modify        //
+// it under the terms of the Lesser GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or             //
+// (at your option) any later version.                                           //
+//                                                                               //
+// Quimera Engine is distributed in the hope that it will be useful,             //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// Lesser GNU General Public License for more details.                           //
+//                                                                               //
+// You should have received a copy of the Lesser GNU General Public License      //
+// along with Quimera Engine. If not, see <http://www.gnu.org/licenses/>.        //
+//                                                                               //
+// This license doesn't force you to put any kind of banner or logo telling      //
+// that you are using Quimera Engine in your project but we would appreciate     //
+// if you do so or, at least, if you let us know about that.                     //
+//                                                                               //
+// Enjoy!                                                                        //
+//                                                                               //
+// Kinesis Team                                                                  //
+//-------------------------------------------------------------------------------//
+
+#ifndef __QFIXEDARRAY__
+#define __QFIXEDARRAY__
+
+#include "DataTypesDefinitions.h"
+#include "ToolsDefinitions.h"
+#include "Assertions.h"
+#include "QPoolAllocator.h"
+#include "QAlignment.h"
+#include "QComparatorDefault.h"
+#include "AllocationOperators.h"
+
+
+using Kinesis::QuimeraEngine::Common::DataTypes::pointer_uint_q;
+using Kinesis::QuimeraEngine::Common::Memory::QPoolAllocator;
+using Kinesis::QuimeraEngine::Common::Memory::QAlignment;
+using Kinesis::QuimeraEngine::Tools::Containers::QComparatorDefault;
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace Tools
+{
+namespace Containers
+{
+
+/// <summary>
+/// Class that represents a contiguous sequence of elements that can be arbitrarly accessed using a zero-based
+/// index. Their size is defined when it is constructed and cannot be changed afterwards.
+/// </summary>
+/// <remarks>
+/// Elements are forced to implement assignment operator, copy constructor and destructor, all of them publicly accessible.
+/// </remarks>
+template <class T, class Allocator = QPoolAllocator, class Comparator = QComparatorDefault<T> >
+class QFixedArray
+{
+protected:
+
+   	// CONSTANTS
+	// ---------------
+
+	/// <summary>
+	/// Constant to symbolize the end of the sequence near the last element.
+	/// </summary>
+    static const pointer_uint_q END_POSITION_FORWARD = -1;
+
+	/// <summary>
+	/// Constant to symbolize the end of the sequence near the first element.
+	/// </summary>
+    static const pointer_uint_q END_POSITION_BACKWARD = -2;
+
+   
+	// CONSTRUCTORS
+	// ---------------
+
+public:
+    
+	/// <summary>
+	/// Constructs a fixed array passing the number of elements to store and their initial value.
+	/// </summary>
+    /// <remarks>
+    /// The copy constructor is called for every element of the array.
+    /// </remarks>
+    /// <param name="uCount"> [IN] Number of elements to store. It must be greater than zero.</param>
+    /// <param name="initialValue"> [IN] The initial value to assign to all array elements.</param>
+    QFixedArray(const pointer_uint_q uCount, const T &initialValue) :
+            m_uFirst(0), 
+            m_uLast(uCount - 1)
+    {
+        QE_ASSERT( uCount > 0, "Zero elements array is not allowed." );
+        QE_ASSERT( this->MultiplicationOverflows(uCount, sizeof(T)) == false, "The amount of memory requested overflows." );
+
+        m_pAllocator = new Allocator(uCount * sizeof(T), sizeof(T), QAlignment(alignof_q(T))); 
+
+        for(pointer_uint_q uIndex = 0; uIndex < uCount; uIndex++)
+        {
+            // Allocates and writes in the returned buffer the initial value
+            new(m_pAllocator->Allocate()) T(initialValue);
+        }
+    }
+
+    /// <summary>
+	/// Copy constructor. Creates a fixed array from another.
+	/// </summary>
+    /// <remarks>
+    /// The copy constructor is called for every element of the array.
+    /// </remarks>
+    /// <param name="fixedArray"> [IN] Fixed array from which to copy.</param>
+    QFixedArray(const QFixedArray &fixedArray) :
+            m_uFirst(fixedArray.m_uFirst),
+            m_uLast(fixedArray.m_uLast)
+    {
+        // [TODO] : rdelasheras. Replace "fixedArray.m_uLast + 1" with "fixedArray.GetCount()" when it exists.
+        m_pAllocator = new Allocator((fixedArray.m_uLast + 1) * sizeof(T), sizeof(T), QAlignment(alignof_q(T)));
+
+        for(pointer_uint_q uIndex = 0; uIndex < fixedArray.m_uLast + 1; uIndex++)
+        {
+            // Constructs a T object over the buffer returned by the allocator and initializes it with
+            // the value of the origin element in the corresponding array position.
+            // [TODO] : rdelasheras. Access to every element with "fixedArray[uIndex]" when it exists instead of using pointers arithmetic.
+            new(m_pAllocator->Allocate()) 
+                T(*(T*)((pointer_uint_q)fixedArray.GetAllocator()->GetPointer() + uIndex * sizeof(T)));
+        }
+    }
+
+protected:       
+
+	/// <summary>
+	/// Constructs an empty and invalid fixed array. It's responsability of the derived classes to initialize it properly.
+	/// </summary>
+	QFixedArray() :
+        m_uFirst(END_POSITION_BACKWARD), 
+        m_uLast(END_POSITION_FORWARD), 
+        m_pAllocator(null_q)
+    {
+    }
+
+    // DESTRUCTOR
+	// ---------------
+public:
+	/// <summary>
+	/// Destructor.
+	/// </summary>		
+    /// <remarks>
+    /// The destructor is called for every element of the array.
+    /// </remarks>
+	~QFixedArray()
+    {
+        QE_ASSERT( null_q != m_pAllocator, "Allocator is null" );
+ 
+        // [TODO] : rdelasheras. Replace "m_uLast + 1" with "this->GetCount()" when it exists.
+        // [TODO] : rdelasheras. Access to every element with "(*this)[uIndex]" when it exists instead of using pointers arithmetic.
+        for(pointer_uint_q uIndex = 0; uIndex < m_uLast + 1; uIndex++)
+            ((T*)((pointer_uint_q)m_pAllocator->GetPointer() + uIndex * sizeof(T)))->~T();
+            
+
+        delete m_pAllocator;
+    }
+
+
+	// METHODS
+	// ---------------
+public:
+  	/// <summary>
+	/// Defines the assignment operator which makes a copy of the passed fixed array. 
+	/// </summary>
+    /// <remarks>
+    /// If the size of the destination array is less than the size of the origin array, it will copy
+    /// only the first elements that fit in the destination array. The exceeding elements are not copied and
+    /// the size of the destination array does not change.
+    /// If the size of the destination array is greater than the size of the origin array, the remaining
+    /// elements in the destination array does not change.
+    /// Assignment operator will be called for every element of the array.
+    /// </remarks>
+    /// <param name="fixedArray"> [IN] Fixed array from which to copy.</param>
+    /// <returns>
+    /// A reference to the resulting fixed array.
+    /// </returns>
+    QFixedArray<T, Allocator, Comparator>& operator= (const QFixedArray<T, Allocator, Comparator> &fixedArray)
+    {
+        pointer_uint_q uElementsToCopy;
+
+        // [TODO] : rdelasheras. Replace "m_uLast + 1" with "GetCount()" when it exists.
+        if( m_uLast > fixedArray.m_uLast )
+            uElementsToCopy = fixedArray.m_uLast + 1;
+        else
+            uElementsToCopy = m_uLast + 1;
+
+        for( pointer_uint_q uIndex = 0; uIndex < uElementsToCopy; uIndex++ )
+        {
+            // Destination array element value = Origin array element value, using pointers arithmetic
+            // [TODO] : rdelasheras. Access to every element with "[uIndex]" when it exists instead of using pointers arithmetic.
+            *(T*)((pointer_uint_q)m_pAllocator->GetPointer() + uIndex * sizeof(T)) =
+                *(T*)((pointer_uint_q)fixedArray.m_pAllocator->GetPointer() + uIndex * sizeof(T));
+        }
+
+        return *this;
+    }
+
+private:
+    
+    /// <sumary>
+    /// Checks if the multiplication of two operands overflows for the pointer_uint_q type.
+    /// </sumary>
+    /// <param name="uOperandA"> [IN] Operand to multiply.</param>
+    /// <param name="uOperandB"> [IN] Operand to multiply.</param>
+    /// <returns>
+    /// True if the result overflows for the pointer_uint_q type.
+    /// </returns>
+    bool MultiplicationOverflows(pointer_uint_q uOperandA, pointer_uint_q uOperandB) const 
+    {
+        return this->HighestOneBitPosition(uOperandA) + this->HighestOneBitPosition(uOperandB) > sizeof(pointer_uint_q) * 8;
+    }
+
+    /// <sumary>
+    /// Gets the highest one bit position of a number converted to binary format.
+    /// </sumary>
+    /// <param name="uValue"> [IN] Number to trate.</param>
+    /// <returns>
+    /// The highest one bit position of the number passed by parameter.
+    /// </returns>
+    pointer_uint_q HighestOneBitPosition(pointer_uint_q uValue) const
+    {
+        pointer_uint_q position = 0;
+
+        while(uValue != 0)
+        {
+            ++position;
+            uValue>>=1;
+        }
+
+        return position;
+    }
+
+
+    // PROPERTIES
+	// ---------------
+public:
+   	/// <summary>
+	/// Returns a constant pointer to the allocator.
+	/// </summary>
+    /// <returns>
+    /// Constant pointer to the allocator. 
+    /// </returns>
+    const Allocator* GetAllocator() const
+    {
+        return m_pAllocator; 
+    }
+
+
+    // ATTRIBUTES
+	// ---------------
+protected:
+	/// <summary>
+	/// Pointer to the allocator which stores the array elements.
+	/// </summary>		
+    Allocator* m_pAllocator;
+    
+    /// <summary>
+	/// The Comparator.
+	/// </summary>		
+    Comparator m_comparator;
+
+    /// <summary>
+	/// Index of the first element in the sequence. If the index points to an invalid position its value is the 
+    /// END_POSITION_BACKWARD constant.
+	/// </summary>		
+    pointer_uint_q m_uFirst;
+
+   	/// <summary>
+	/// Index of the last element in the sequence. If the index points to an invalid position its value is the 
+    /// END_POSITION_FORWARD constant.
+	/// </summary>		
+    pointer_uint_q m_uLast;
+
+};
+
+} //namespace Containers
+} //namespace Tools
+} //namespace QuimeraEngine
+} //namespace Kinesis
+
+#endif // __QFIXEDARRAY__
---
+++ trunk/testing/tests/unit/testmodule_tools/QFixedArray_test.cpp
@@ -0,0 +1,359 @@
+//-------------------------------------------------------------------------------//
+//                         QUIMERA ENGINE : LICENSE                              //
+//-------------------------------------------------------------------------------//
+// This file is part of Quimera Engine.                                          //
+// Quimera Engine is free software: you can redistribute it and/or modify        //
+// it under the terms of the Lesser GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or             //
+// (at your option) any later version.                                           //
+//                                                                               //
+// Quimera Engine is distributed in the hope that it will be useful,             //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// Lesser GNU General Public License for more details.                           //
+//                                                                               //
+// You should have received a copy of the Lesser GNU General Public License      //
+// along with Quimera Engine. If not, see <http://www.gnu.org/licenses/>.        //
+//                                                                               //
+// This license doesn't force you to put any kind of banner or logo telling      //
+// that you are using Quimera Engine in your project but we would appreciate     //
+// if you do so or, at least, if you let us know about that.                     //
+//                                                                               //
+// Enjoy!                                                                        //
+//                                                                               //
+// Kinesis Team                                                                  //
+//-------------------------------------------------------------------------------//
+
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+using namespace boost::unit_test;
+
+#include "../../testsystem/TestingExternalDefinitions.h"
+
+#include "QFixedArray.h"
+#include "QFixedArrayWhiteBox.h"
+#include "ArrayElementMock.h"
+
+using Kinesis::QuimeraEngine::Common::DataTypes::u64_q;
+using Kinesis::QuimeraEngine::Tools::Containers::QFixedArray;
+using Kinesis::QuimeraEngine::Tools::Containers::Test::QFixedArrayWhiteBox;
+using Kinesis::QuimeraEngine::Tools::Containers::Test::ArrayElementMock;
+
+i16_q ArrayElementMock::m_nNumberOfAllocatedObjects = 0;
+
+QTEST_SUITE_BEGIN( QFixedArray_TestSuite )
+
+
+#if QE_CONFIG_ASSERTSBEHAVIOR_DEFAULT == QE_CONFIG_ASSERTSBEHAVIOR_THROWEXCEPTIONS
+
+/// <summary>
+/// Checks than assertion fails when attempting to create an array with zero elements.
+/// </summary>
+QTEST_CASE ( Constructor1_AssertionFailsWhenCreatingArrayWithZeroElements_Test )
+{
+    // [Preparation]
+    const u64_q INITIAL_VALUE = 12;
+    const pointer_uint_q ZERO_ARRAY_COUNT = 0;
+    const bool ASSERTION_FAILED = true;
+
+    bool bAssertionFailed = false;
+
+    // [Execution]
+
+    try
+    {
+        QFixedArray<u64_q> fixedArray = QFixedArray<u64_q>(ZERO_ARRAY_COUNT, INITIAL_VALUE);
+    }
+    catch(...)
+    {
+        bAssertionFailed = true;
+    }
+
+    // [Verification]
+    BOOST_CHECK_EQUAL( bAssertionFailed, ASSERTION_FAILED );
+}
+
+#endif // QE_CONFIG_ASSERTSBEHAVIOR_DEFAULT == QE_CONFIG_ASSERTSBEHAVIOR_THROWEXCEPTIONS
+
+/// <summary>
+/// Checks if it's correctly initialized with initial value.
+/// </summary>
+QTEST_CASE ( Constructor1_ChecksifIsCorrectlyInitializedWithInitialValue_Test )
+{
+    // [Preparation]
+    const u64_q INITIAL_VALUE = 12;
+    const pointer_uint_q ARRAY_COUNT = 10;
+    const pointer_uint_q SIZEOF_VALUE_TYPE = sizeof(u64_q);
+    const bool VALUES_ARE_AS_EXPECTED = true;
+
+    // [Execution]
+    QFixedArray<u64_q> fixedArray = QFixedArray<u64_q>(ARRAY_COUNT, INITIAL_VALUE);
+
+    // [Verification]
+    u64_q uValue;
+    bool bValuesAreAsExpected = true;
+
+    for(pointer_uint_q uIndex = 0; uIndex < ARRAY_COUNT; uIndex++ )
+    {
+        // [TODO] rdelasheras : Replace with operator[] when it is ready.
+        uValue = *((u64_q*)((pointer_uint_q)fixedArray.GetAllocator()->GetPointer() + uIndex * SIZEOF_VALUE_TYPE));
+
+        bValuesAreAsExpected &= ( uValue == INITIAL_VALUE );
+    }
+
+    BOOST_CHECK_EQUAL( bValuesAreAsExpected, VALUES_ARE_AS_EXPECTED );
+}
+
+/// <summary>
+/// Checks if the allocator has the expected size.
+/// </summary>
+QTEST_CASE ( Constructor1_AllocatorHasExpectedSize_Test )
+{
+    // [Preparation]
+    const u64_q INITIAL_VALUE = 12;
+    const pointer_uint_q ARRAY_COUNT = 10;
+    const pointer_uint_q SIZEOF_VALUE_TYPE = sizeof(u64_q);
+    const pointer_uint_q ALLOCATOR_EXPECTED_SIZE = ARRAY_COUNT * SIZEOF_VALUE_TYPE;
+
+    // [Execution]
+    QFixedArray<u64_q> fixedArray = QFixedArray<u64_q>(ARRAY_COUNT, INITIAL_VALUE);
+
+    // [Verification]
+    pointer_uint_q uAllocatorSize = fixedArray.GetAllocator()->GetPoolSize();
+
+    BOOST_CHECK_EQUAL( uAllocatorSize, ALLOCATOR_EXPECTED_SIZE );
+}
+
+/// <summary>
+/// Checks if the copy constructor of the array elements are called.
+/// </summary>
+QTEST_CASE ( Constructor1_CopyConstructorOfArrayElementsAreCalled_Test )
+{
+    // [Preparation]
+    const ArrayElementMock INITIAL_VALUE = ArrayElementMock(12);
+    const pointer_uint_q ARRAY_COUNT = 10;
+    const pointer_uint_q SIZEOF_VALUE_TYPE = sizeof(INITIAL_VALUE);
+    const pointer_uint_q EXPECTED_CALLS_TO_COPY_CONSTRUCTOR = ARRAY_COUNT;
+
+    // [Execution]
+    QFixedArray<ArrayElementMock> fixedArray = QFixedArray<ArrayElementMock>(ARRAY_COUNT, INITIAL_VALUE);
+
+    // [Verification]
+    pointer_uint_q uCallsToCopyConstructor = 0;
+
+    for(pointer_uint_q uIndex = 0; uIndex < ARRAY_COUNT; uIndex++)
+    {
+        if(((ArrayElementMock*)
+            ((pointer_uint_q)fixedArray.GetAllocator()->GetPointer() + uIndex * sizeof(ArrayElementMock)))
+            ->GetCallToCopyConstructor())
+        {
+            uCallsToCopyConstructor++;
+        }
+    }
+
+    BOOST_CHECK_EQUAL( uCallsToCopyConstructor, EXPECTED_CALLS_TO_COPY_CONSTRUCTOR );
+}
+
+/// <summary>
+/// Checks if the destructor of the array elements are called.
+/// </summary>
+QTEST_CASE ( Constructor1_DestructorOfArrayElementsAreCalled_Test )
+{
+    // [Preparation]
+    const ArrayElementMock INITIAL_VALUE = ArrayElementMock(12);
+    const pointer_uint_q ARRAY_COUNT = 10;
+    const pointer_uint_q SIZEOF_VALUE_TYPE = sizeof(INITIAL_VALUE);
+    const i16_q EXPECTED_REMAINING_OBJECTS_ALLOCATED = 1;
+
+    // [Execution]
+    QFixedArray<ArrayElementMock> *pFixedArray = new QFixedArray<ArrayElementMock>(ARRAY_COUNT, INITIAL_VALUE);
+    delete pFixedArray;
+
+    // [Verification]
+    i16_q nRemainingObjectsAllocated = ArrayElementMock::GetNumberOfAllocatedObjects();
+
+    BOOST_CHECK_EQUAL( nRemainingObjectsAllocated, EXPECTED_REMAINING_OBJECTS_ALLOCATED );
+}
+
+/// <summary>
+/// Checks if the copy constructor of the array elements are called for the second constructor.
+/// </summary>
+QTEST_CASE ( Constructor2_CopyConstructorOfArrayElementsAreCalled_Test )
+{
+    // [Preparation]
+    const ArrayElementMock INITIAL_VALUE = ArrayElementMock(12);
+    const pointer_uint_q ARRAY_COUNT = 10;
+    const pointer_uint_q SIZEOF_VALUE_TYPE = sizeof(INITIAL_VALUE);
+    const pointer_uint_q EXPECTED_CALLS_TO_COPY_CONSTRUCTOR = ARRAY_COUNT;
+    
+    QFixedArray<ArrayElementMock> fixedArrayOrigin = QFixedArray<ArrayElementMock>(ARRAY_COUNT, INITIAL_VALUE);
+
+    // [Execution]
+    QFixedArray<ArrayElementMock> fixedArrayDestination = QFixedArray<ArrayElementMock>(fixedArrayOrigin);
+
+    // [Verification]
+    pointer_uint_q uCallsToCopyConstructor = 0;
+
+    for(pointer_uint_q uIndex = 0; uIndex < ARRAY_COUNT; uIndex++)
+    {
+        if(((ArrayElementMock*)
+            ((pointer_uint_q)fixedArrayDestination.GetAllocator()->GetPointer() + uIndex * sizeof(ArrayElementMock)))
+            ->GetCallToCopyConstructor())
+        {
+            uCallsToCopyConstructor++;
+        }
+    }
+
+    BOOST_CHECK_EQUAL( uCallsToCopyConstructor, EXPECTED_CALLS_TO_COPY_CONSTRUCTOR );
+}
+
+
+
+/// <summary>
+/// Checks if the constructor without parameters initializes correctly.
+/// </summary>
+QTEST_CASE ( Constructor3_ConstructorWithoutParametersInitializesCorrectly_Test )
+{
+    // [Preparation]
+ 
+    const pointer_uint_q END_POSITION_FORWARD = -1;
+    const pointer_uint_q END_POSITION_BACKWARD = -2;
+
+    // [Execution]
+
+    QFixedArrayWhiteBox<u64_q> fixedArray;
+
+    // [Verification]
+
+    pointer_uint_q uFirst = fixedArray.GetFirst();
+    pointer_uint_q uLast  = fixedArray.GetLast();
+
+    BOOST_CHECK_EQUAL( uFirst, END_POSITION_BACKWARD );
+    BOOST_CHECK_EQUAL( uLast, END_POSITION_FORWARD );
+}
+
+
+/// <summary>
+/// Checks if it calls to operator assignment of the array elements.
+/// </summary>
+QTEST_CASE ( OperatorAssignment_OperatorAssignmentOfArrayElementsIsCalled_Test )
+{
+    // [Preparation]
+    const ArrayElementMock INITIAL_VALUE = ArrayElementMock(12);
+    const pointer_uint_q ARRAY_COUNT = 10;
+    const pointer_uint_q EXPECTED_CALLS_TO_OPERATOR_ASSIGNMENT = ARRAY_COUNT;
+    
+    QFixedArray<ArrayElementMock> fixedArrayOrigin = QFixedArray<ArrayElementMock>(ARRAY_COUNT, INITIAL_VALUE);
+    QFixedArray<ArrayElementMock> fixedArrayDestination = QFixedArray<ArrayElementMock>(ARRAY_COUNT, INITIAL_VALUE);
+            
+    // [Execution]
+    fixedArrayDestination = fixedArrayOrigin;
+
+    // [Verification]
+    pointer_uint_q uCallsToOperatorAssignment = 0;
+
+    for(pointer_uint_q uIndex = 0; uIndex < ARRAY_COUNT; uIndex++)
+    {
+        if(((ArrayElementMock*)
+            ((pointer_uint_q)fixedArrayDestination.GetAllocator()->GetPointer() + uIndex * sizeof(ArrayElementMock)))
+            ->GetCallToOperatorAssignment())
+        {
+            uCallsToOperatorAssignment++;
+        }
+    }
+
+    BOOST_CHECK_EQUAL( uCallsToOperatorAssignment, EXPECTED_CALLS_TO_OPERATOR_ASSIGNMENT );
+}
+
+/// <summary>
+/// Checks if it calls operator assignment of elements as number of times as number of elements has the
+/// origin array, when it's less than the destination array.
+/// </summary>
+QTEST_CASE ( OperatorAssignment_OriginSizeNumberOfCopiesAreDoneWhereIsLessThanDestinationSize_Test )
+{
+    // [Preparation]
+    const ArrayElementMock INITIAL_VALUE = ArrayElementMock(12);
+    const pointer_uint_q ARRAY_COUNT_ORIGIN = 5;
+    const pointer_uint_q ARRAY_COUNT_DESTINATION = 10;
+    
+    QFixedArray<ArrayElementMock> fixedArrayOrigin = QFixedArray<ArrayElementMock>(ARRAY_COUNT_ORIGIN, INITIAL_VALUE);
+    QFixedArray<ArrayElementMock> fixedArrayDestination = QFixedArray<ArrayElementMock>(ARRAY_COUNT_DESTINATION, INITIAL_VALUE);
+
+    const pointer_uint_q EXPECTED_CALLS_TO_OPERATOR_ASSIGNMENT = ARRAY_COUNT_ORIGIN;
+
+    // [Execution]
+    fixedArrayDestination = fixedArrayOrigin;
+
+    // [Verification]
+    pointer_uint_q uCallsToOperatorAssignment = 0;
+
+    for(pointer_uint_q uIndex = 0; uIndex < ARRAY_COUNT_DESTINATION; uIndex++)
+    {
+        if(((ArrayElementMock*)
+            ((pointer_uint_q)fixedArrayDestination.GetAllocator()->GetPointer() + uIndex * sizeof(ArrayElementMock)))
+            ->GetCallToOperatorAssignment())
+        {
+            uCallsToOperatorAssignment++;
+        }
+    }
+
+    BOOST_CHECK_EQUAL( uCallsToOperatorAssignment, EXPECTED_CALLS_TO_OPERATOR_ASSIGNMENT );
+
+}
+
+/// <summary>
+/// Checks if it calls operator assignment of elements as number of times as number of elements has the
+/// destination array, when the origin array is greater than the destination array.
+/// </summary>
+QTEST_CASE ( OperatorAssignment_DestinationSizeNumberOfCopiesAreDoneWhereOriginSizeIsGreatherThanDestinationSize_Test )
+{
+    // [Preparation]
+    const ArrayElementMock INITIAL_VALUE = ArrayElementMock(12);
+    const pointer_uint_q ARRAY_COUNT_ORIGIN = 10;
+    const pointer_uint_q ARRAY_COUNT_DESTINATION = 5;
+    
+    QFixedArray<ArrayElementMock> fixedArrayOrigin = QFixedArray<ArrayElementMock>(ARRAY_COUNT_ORIGIN, INITIAL_VALUE);
+    QFixedArray<ArrayElementMock> fixedArrayDestination = QFixedArray<ArrayElementMock>(ARRAY_COUNT_DESTINATION, INITIAL_VALUE);
+
+    const pointer_uint_q EXPECTED_CALLS_TO_OPERATOR_ASSIGNMENT = ARRAY_COUNT_DESTINATION;
+
+    // [Execution]
+    fixedArrayDestination = fixedArrayOrigin;
+
+    // [Verification]
+
+    pointer_uint_q uCallsToOperatorAssignment = 0;
+
+    for(pointer_uint_q uIndex = 0; uIndex < ARRAY_COUNT_DESTINATION; uIndex++)
+    {
+        if(((ArrayElementMock*)
+            ((pointer_uint_q)fixedArrayDestination.GetAllocator()->GetPointer() + uIndex * sizeof(ArrayElementMock)))
+            ->GetCallToOperatorAssignment())
+        {
+            uCallsToOperatorAssignment++;
+        }
+    }
+
+    BOOST_CHECK_EQUAL( uCallsToOperatorAssignment, EXPECTED_CALLS_TO_OPERATOR_ASSIGNMENT );
+}
+
+/// <summary>
+/// Checks if returns a valid allocator.
+/// </summary>
+QTEST_CASE ( GetAllocator_ReturnsValidAllocator_Test )
+{
+    // [Preparation]
+    const u64_q INITIAL_VALUE = 12;
+    const pointer_uint_q ARRAY_COUNT = 5;
+
+    // [Execution]
+    QFixedArray<u64_q> fixedArray = QFixedArray<u64_q>(ARRAY_COUNT, INITIAL_VALUE);
+
+    u64_q uFirstValue = *((u64_q*)fixedArray.GetAllocator()->GetPointer());
+
+    // [Verification]
+    BOOST_CHECK_EQUAL( uFirstValue, INITIAL_VALUE );
+}
+
+// End - Test Suite: QFixedArray
+QTEST_SUITE_END()
