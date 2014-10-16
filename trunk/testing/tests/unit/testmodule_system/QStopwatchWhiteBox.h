442ac7a1d931256f33a4d32516d17d8545647a01
lince3d@gmail.com <lince3d@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2014-10-16 23:56:55 +0000

    558 - QStopWatch Done.

---
+++ trunk/testing/tests/unit/testmodule_system/QStopwatchWhiteBox.h
@@ -0,0 +1,69 @@
+//-------------------------------------------------------------------------------//
+//                         QUIMERA ENGINE : LICENSE                              //
+//-------------------------------------------------------------------------------//
+// This file is part of Quimera Engine.                                          //
+// Quimera Engine is free software: you can redistribute it and/or modify        //
+// it under the terms of the Lesser GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or             //
+// (at your option) any later version.                                           //
+//                                                                               //
+// Quimera Engine is distributed in the hope that it will be useful,             //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// Lesser GNU General Public License for more details.                           //
+//                                                                               //
+// You should have received a copy of the Lesser GNU General Public License      //
+// along with Quimera Engine. If not, see <http://www.gnu.org/licenses/>.        //
+//                                                                               //
+// This license doesn't force you to put any kind of banner or logo telling      //
+// that you are using Quimera Engine in your project but we would appreciate     //
+// if you do so or, at least, if you let us know about that.                     //
+//                                                                               //
+// Enjoy!                                                                        //
+//                                                                               //
+// Kinesis Team                                                                  //
+//-------------------------------------------------------------------------------//
+
+#ifndef __QSTOPWATCHWHITEBOX__
+#define __QSTOPWATCHWHITEBOX__
+
+#include "QStopwatch.h"
+
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace System
+{
+namespace Timing
+{
+namespace Test
+{
+
+/// <summary>
+/// Exposes the protected methods of the QStopwatch to perform white-box tests.
+/// </summary>
+class QStopwatchWhiteBox : public QStopwatch
+{
+
+    // METHODS
+	// ---------------
+public:
+
+    // Method to obtain the reference instant
+    u64_q GetReferenceInstantForTest()
+    {
+        return m_uReferenceTime;
+    }
+
+};
+
+} //namespace Test
+} //namespace Timing
+} //namespace System
+} //namespace QuimeraEngine
+} //namespace Kinesis
+
+
+#endif // __QSTOPWATCHWHITEBOX__
