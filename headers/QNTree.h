543c564a46d6a51769b259182b9f157d6f034be8
lince3d <Lince3D@gmail.com>
2014-10-31 18:53:43 +0000

    493 - QNTree 006 Done.

--- headers/QNTree.h
+++ headers/QNTree.h
@@ -1197,6 +1197,87 @@ public:
 
         return *this;
     }
+    
+    /// <summary>
+    /// Equality operator that checks whether two trees are equal.
+    /// </summary>
+    /// <remarks>
+    /// Every element is compared with the element at the same position in the other tree. Elements are compared using the tree's comparator.<br/>
+    /// Trees are compared morphologically too, so if an element A is a child of another element B in one of the trees, this relation must exist in the other tree to be considered equal.
+    /// </remarks>
+    /// <param name="tree">[IN] The tree to compare to.</param>
+    /// <returns>
+    /// True if all the elements of both trees are equal and the trees have the same structure; False otherwise.
+    /// </returns>
+    bool operator==(const QNTree &tree) const
+    {
+        bool bAreEqual = true;
+
+        // If they are not the same instance
+        if(this != &tree)
+        {
+            // If they have the same number of elements
+            if(this->GetCount() == tree.GetCount())
+            {
+                // Both trees are traversed in the same order
+                QNTree::QNTreeIterator itThis = this->GetRoot(EQTreeTraversalOrder::E_DepthFirstPreOrder);
+                QNTree::QNTreeIterator itInput = tree.GetRoot(EQTreeTraversalOrder::E_DepthFirstPreOrder);
+
+                QNTree::QNode* pNodeBasePointerThis  = scast_q(m_nodeAllocator.GetPointer(), QNTree::QNode*);
+                QNTree::QNode* pNodeBasePointerInput = scast_q(tree.m_nodeAllocator.GetPointer(), QNTree::QNode*);
+                T* pElementBasePointerThis  = scast_q(m_elementAllocator.GetPointer(), T*);
+                T* pElementBasePointerInput = scast_q(tree.m_elementAllocator.GetPointer(), T*);
+
+                QNTree::QNode* pCurrentNodeThis = null_q;
+                QNTree::QNode* pCurrentNodeInput = null_q;
+
+                pointer_uint_q uCurrentNodePositionThis = 0;
+                pointer_uint_q uCurrentNodePositionInput = 0;
+
+                while(bAreEqual && !itThis.IsEnd())
+                {
+                    // Note: Both trees can have the same structure and be physically ordered differently in memory
+
+                    uCurrentNodePositionThis = &*itThis - pElementBasePointerThis;
+                    uCurrentNodePositionInput = &*itInput - pElementBasePointerInput;
+
+                    pCurrentNodeThis = pNodeBasePointerThis + uCurrentNodePositionThis;
+                    pCurrentNodeInput = pNodeBasePointerInput + uCurrentNodePositionInput;
+
+                    // The value of the current element is checked
+                    bAreEqual = m_comparator.Compare(*itThis, *itInput) == 0
+                    // The relations of the current node are checked too
+                                && ((pCurrentNodeThis->GetFirstChild() != QNTree::END_POSITION_FORWARD) == (pCurrentNodeInput->GetFirstChild() != QNTree::END_POSITION_FORWARD))
+                                && ((pCurrentNodeThis->GetNext() != QNTree::END_POSITION_FORWARD) == (pCurrentNodeInput->GetNext() != QNTree::END_POSITION_FORWARD))
+                                && ((pCurrentNodeThis->GetPrevious() != QNTree::END_POSITION_FORWARD) == (pCurrentNodeInput->GetPrevious() != QNTree::END_POSITION_FORWARD))
+                                && ((pCurrentNodeThis->GetParent() != QNTree::END_POSITION_FORWARD) == (pCurrentNodeInput->GetParent() != QNTree::END_POSITION_FORWARD));
+
+                    ++itThis;
+                    ++itInput;
+                }
+            }
+            else
+                bAreEqual = false;
+        }
+
+        return bAreEqual;
+    }
+    
+    /// <summary>
+    /// Inequality operator that checks whether two trees are not equal.
+    /// </summary>
+    /// <remarks>
+    /// Every element is compared with the element at the same position in the other tree. Elements are compared using the tree's comparator.<br/>
+    /// Trees are compared morphologically too, so if an element A is a child of another element B in one of the trees, this relation must exist in the other tree to be considered equal.
+    /// </remarks>
+    /// <param name="tree">[IN] The tree to compare to.</param>
+    /// <returns>
+    /// True if not all the elements of both trees are equal or if the trees do not have the same structure; False otherwise.
+    /// </returns>
+    bool operator!=(const QNTree &tree) const
+    {
+        return !this->operator==(tree);
+    }
 
     /// <summary>
     /// Increases the capacity of the tree, reserving memory for more elements.
@@ -1882,7 +1963,29 @@ public:
 
         return itElement;
     }
+    
+    /// <summary>
+    /// Searches for a given element, starting at a concrete point, and obtains its position.
+    /// </summary>
+    /// <param name="element">[IN] The value of the element to search for.</param>
+    /// <param name="eTraversalOrder">[IN] The order in which the elements of the tree will be visited.</param>
+    /// <param name="startPosition">[IN] An iterator that points to the node from which to start searching. It must not point to an end position.</param>
+    /// <returns>
+    /// An iterator that points to the position of the first occurrence of the element, starting at the given position, depending on the traversal order. If the element is not found, 
+    /// the iterator will point to the end position.
+    /// </returns>
+    QNTreeIterator PositionOf(const T &element, const EQTreeTraversalOrder &eTraversalOrder, const typename QNTree::QNTreeIterator startPosition) const
+    {
+        QE_ASSERT_ERROR(!startPosition.IsEnd(), "The start position must not point to the end position.");
 
+        QNTree::QNTreeIterator itElement = startPosition;
+
+        while(!itElement.IsEnd() && m_comparator.Compare(*itElement, element) != 0)
+            ++itElement;
+
+        return itElement;
+    }
+    
     /// <summary>
     /// Gets the first child in the child list of a given node.
     /// </summary>
