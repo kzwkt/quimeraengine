06b01f41e2bf27b7ba0b44bce7e50f1dbd81d170
lince3d@gmail.com <lince3d@gmail.com@7af85c2e-922d-2424-809b-042d02d0a1c2>
2014-11-05 02:33:00 +0000

    Some tests fixed to make them pass on Mac OS.

--- trunk/testing/tests/unit/testmodule_tools/QVector4_Test.cpp
+++ trunk/testing/tests/unit/testmodule_tools/QVector4_Test.cpp
@@ -3384,13 +3384,13 @@ QTEST_CASE ( ToString_ReturnedFormatMatchesExpected_Test )
     const QVector4 VECTOR = QVector4(SQFloat::_0_25, (float_q)-0.000002, (float_q)40000.0, SQFloat::_1);
 
 #if QE_CONFIG_PRECISION_DEFAULT == QE_CONFIG_PRECISION_SIMPLE
-    #if defined(QE_COMPILER_GCC) && defined(QE_OS_LINUX) // This is necessary due to a different implementation of the STL when compiling with GCC on Linux
+    #if defined(QE_COMPILER_GCC) && (defined(QE_OS_LINUX) || defined(QE_OS_MAC)) // This is necessary due to a different implementation of the Standard Library when compiling with GCC on Linux
         string_q EXPECTED_STRING_FORM = QE_L("V4(0.25,-1.99999999e-06,40000,1)");
     #else
         string_q EXPECTED_STRING_FORM = QE_L("V4(0.25,-1.99999999e-006,40000,1)");
     #endif
 #elif QE_CONFIG_PRECISION_DEFAULT == QE_CONFIG_PRECISION_DOUBLE
-    #if defined(QE_COMPILER_GCC) && defined(QE_OS_LINUX) // This is necessary due to a different implementation of the STL when compiling with GCC on Linux
+    #if defined(QE_COMPILER_GCC) && (defined(QE_OS_LINUX) || defined(QE_OS_MAC)) // This is necessary due to a different implementation of the Standard Library when compiling with GCC on Linux
         string_q EXPECTED_STRING_FORM = QE_L("V4(0.25,-1.9999999999999999e-06,40000,1)");
     #else
         string_q EXPECTED_STRING_FORM = QE_L("V4(0.25,-1.9999999999999999e-006,40000,1)");
