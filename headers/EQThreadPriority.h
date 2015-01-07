97d8606475f18eacd82d2c3f9c96ccd8b3a7d4af
lince3d <Lince3D@gmail.com>
2015-01-07 04:03:07 +0000

    564 - SQThisThread 001 Done. It's pending to test it on Linux and Mac. Added the EQThreadPriority enumeration.

---
+++ headers/EQThreadPriority.h
@@ -0,0 +1,360 @@
+//-------------------------------------------------------------------------------//
+//                         QUIMERA ENGINE : LICENSE                              //
+//-------------------------------------------------------------------------------//
+// This file is part of Quimera Engine.                                          //
+// Quimera Engine is free software: you can redistribute it and/or modify        //
+// it under the terms of the Lesser GNU General Public License as published by   //
+// the Free Software Foundation, either version 3 of the License, or             //
+// (at your option) any later version.                                           //
+//                                                                               //
+// Quimera Engine is distributed in the hope that it will be useful,             //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// Lesser GNU General Public License for more details.                           //
+//                                                                               //
+// You should have received a copy of the Lesser GNU General Public License      //
+// along with Quimera Engine. If not, see <http://www.gnu.org/licenses/>.        //
+//                                                                               //
+// This license doesn't force you to put any kind of banner or logo telling      //
+// that you are using Quimera Engine in your project but we would appreciate     //
+// if you do so or, at least, if you let us know about that.                     //
+//                                                                               //
+// Enjoy!                                                                        //
+//                                                                               //
+// Kinesis Team                                                                  //
+//-------------------------------------------------------------------------------//
+
+#ifndef __EQTHREADPRIORITY__
+#define __EQTHREADPRIORITY__
+
+#include <map>
+#include <vector>
+
+#include "Assertions.h"
+#include "DataTypesDefinitions.h"
+#include "SystemDefinitions.h"
+
+#ifdef QE_COMPILER_MSVC
+    // This warning appears when instancing a template to create a data member and that template instance is not exported.
+    // In this case, it is not important since the data member is not accessible.
+    #pragma warning( disable : 4251 ) // http://msdn.microsoft.com/en-us/library/esew7y1w.aspx
+#endif
+
+using Kinesis::QuimeraEngine::Common::DataTypes::string_q;
+using Kinesis::QuimeraEngine::Common::DataTypes::enum_int_q;
+
+
+namespace Kinesis
+{
+namespace QuimeraEngine
+{
+namespace System
+{
+namespace Threading
+{
+
+/// <summary>
+/// The priority the operating system gives to a thread.
+/// </summary>
+class QE_LAYER_SYSTEM_SYMBOLS EQThreadPriority
+{
+    // ENUMERATIONS
+    // ---------------
+public:
+
+    /// <summary>
+    /// The encapsulated enumeration.
+    /// </summary>
+    enum EnumType
+    {
+        E_Lowest = QE_ENUMERATION_MIN_VALUE, /*!< The lowest priority. */
+        E_Low,                               /*!< Low priority. */
+        E_Normal,                            /*!< Normal priority. */
+        E_High,                              /*!< High priority. */
+        E_Highest,                           /*!< The highest priority. */
+        _NotEnumValue = QE_ENUMERATION_MAX_VALUE /*!< Not valid value. */
+    };
+
+    // TYPEDEFS
+    // ---------------
+public:
+
+    typedef std::map<string_q, EQThreadPriority::EnumType> TNameValueMap;
+    typedef std::pair<string_q, EQThreadPriority::EnumType> TNameValuePair;
+
+
+	// CONSTRUCTORS
+	// ---------------
+public:
+
+    /// <summary>
+    /// Constructor that receives a valid enumeration value.
+    /// </summary>
+    /// <param name="eValue">[IN] A valid enumeration value.</param>
+    inline EQThreadPriority(const EQThreadPriority::EnumType eValue) : m_value(eValue)
+    {
+    }
+
+    /// <summary>
+    /// Constructor that receives an integer number which must correspond to a valid enumeration value.
+    /// </summary>
+    /// <param name="nValue">[IN] An integer number.</param>
+    inline EQThreadPriority(const enum_int_q nValue) : m_value(scast_q(nValue, const EQThreadPriority::EnumType))
+    {
+    }
+
+    /// <summary>
+    /// Constructor that receives the name of a valid enumeration value. Note that enumeration value names don't include
+    /// the enumeration prefix.
+    /// </summary>
+    /// <param name="strValueName">[IN] The name of a valid enumeration value.</param>
+    inline explicit EQThreadPriority(const string_q &strValueName)
+    {
+        *this = strValueName;
+    }
+    
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="eValue">[IN] Another enumeration.</param>
+    inline EQThreadPriority(const EQThreadPriority &eValue) : m_value(eValue.m_value)
+    {
+    }
+
+
+	// METHODS
+	// ---------------
+public:
+
+    /// <summary>
+    /// Assign operator that accepts an integer number that corresponds to a valid enumeration value.
+    /// </summary>
+    /// <param name="nValue">[IN] An integer number.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    inline EQThreadPriority& operator=(const enum_int_q nValue)
+    {
+        m_value = scast_q(nValue, const EQThreadPriority::EnumType);
+        return *this;
+    }
+
+    /// <summary>
+    /// Assign operator that accepts a valid enumeration value name.
+    /// </summary>
+    /// <param name="strValueName">[IN] The enumeration value name.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    inline EQThreadPriority& operator=(const string_q &strValueName)
+    {
+        if(EQThreadPriority::sm_mapValueName.find(strValueName) != EQThreadPriority::sm_mapValueName.end())
+            m_value = sm_mapValueName[strValueName];
+        else
+            m_value = EQThreadPriority::_NotEnumValue;
+
+        return *this;
+    }
+
+    /// <summary>
+    /// Assign operator that accepts a valid enumeration value.
+    /// </summary>
+    /// <param name="eValue">[IN] A valid enumeration value.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    inline EQThreadPriority& operator=(const EQThreadPriority::EnumType eValue)
+    {
+        m_value = eValue;
+        return *this;
+    }
+    
+    /// <summary>
+    /// Assign operator that accepts another enumeration.
+    /// </summary>
+    /// <param name="eValue">[IN] Another enumeration.</param>
+    /// <returns>
+    /// The enumerated type itself.
+    /// </returns>
+    inline EQThreadPriority& operator=(const EQThreadPriority &eValue)
+    {
+        m_value = eValue.m_value;
+        return *this;
+    }
+    
+    /// <summary>
+    /// Equality operator that receives another enumeration.
+    /// </summary>
+    /// <param name="eValue">[IN] The other enumeration.</param>
+    /// <returns>
+    /// True if it equals the enumeration value. False otherwise.
+    /// </returns>
+    bool operator==(const EQThreadPriority &eValue) const
+    {
+        return m_value == eValue.m_value;
+    }
+
+    /// <summary>
+    /// Equality operator that accepts the name of a valid enumeration value. Note that enumeration value names don't include
+    /// the enumeration prefix.
+    /// </summary>
+    /// <param name="strValueName">[IN] The enumeration value name.</param>
+    /// <returns>
+    /// True if the name corresponds to a valid enumeration value and it equals the contained value. False otherwise.
+    /// </returns>
+    inline bool operator==(const string_q &strValueName) const
+    {
+        if(EQThreadPriority::sm_mapValueName.find(strValueName) != EQThreadPriority::sm_mapValueName.end())
+            return m_value == sm_mapValueName[strValueName];
+        else
+            return false;
+    }
+
+    /// <summary>
+    /// Equality operator that accepts an integer number which must correspond to a valid enumeration value.
+    /// </summary>
+    /// <param name="nValue">[IN] An integer number.</param>
+    /// <returns>
+    /// True if the number corresponds to a valid enumeration value and it equals the contained value. False otherwise.
+    /// </returns>
+    inline bool operator==(const enum_int_q nValue) const
+    {
+        return m_value == scast_q(nValue, const EQThreadPriority::EnumType);
+    }
+
+    /// <summary>
+    /// Equality operator that receives a valid enumeration value.
+    /// </summary>
+    /// <param name="eValue">[IN] The enumeration value.</param>
+    /// <returns>
+    /// True if it equals the contained value. False otherwise.
+    /// </returns>
+    bool operator==(const EQThreadPriority::EnumType eValue) const
+    {
+        return m_value == eValue;
+    }
+    
+    /// <summary>
+    /// Retrieves a list of all the values of the enumeration.
+    /// </summary>
+    /// <returns>
+    /// A list of all the values of the enumeration.
+    /// </returns>
+    static const std::vector<EnumType>& GetValues()
+    {
+        static std::vector<EnumType> arValues;
+
+        // If it's not been initialized yet...
+        if(arValues.empty())
+        {
+            const size_t ENUM_ARRAY_COUNT = EQThreadPriority::sm_mapValueName.size();
+
+            // An empty enumeration makes no sense
+            QE_ASSERT_ERROR(ENUM_ARRAY_COUNT > 0, "An empty enumeration makes no sense");
+
+            for(size_t i = 0; i < ENUM_ARRAY_COUNT; ++i)
+                arValues.push_back(EQThreadPriority::sm_arValueName[i].second);
+        }
+
+        return arValues;
+    }
+
+    /// <summary>
+    /// Casting operator that converts the class capsule into a valid enumeration value.
+    /// </summary>
+    /// <returns>
+    /// The contained enumeration value.
+    /// </returns>
+    inline operator EQThreadPriority::EnumType() const
+    {
+        return m_value;
+    }
+    
+    /// <summary>
+    /// Casting operator that converts the enumerated type value into its corresponding name.
+    /// </summary>
+    /// <returns>
+    /// The contained enumeration value name. If the enumeration value is not valid, then returns an empty string.
+    /// </returns>
+    operator const string_q() const
+    {
+        return ConvertToString(m_value, EQThreadPriority::sm_mapValueName);
+    }
+    
+    /// <summary>
+    /// Converts the enumerated type value into its corresponding integer number.
+    /// </summary>
+    /// <returns>
+    /// The integer number which corresponds to the contained enumeration value.
+    /// </returns>
+    enum_int_q ToInteger() const
+    {
+        return scast_q(m_value, enum_int_q);
+    }
+
+    /// <summary>
+    /// Converts the enumerated type value into its corresponding name.
+    /// </summary>
+    /// <returns>
+    /// The contained enumeration value name. If the enumeration value is not valid, the returns an empty string.
+    /// </returns>
+    const string_q ToString() const
+    {
+        return ConvertToString(m_value, EQThreadPriority::sm_mapValueName);
+    }
+
+private:
+
+    // <summary>
+    // Uses an enumerated value as a key to retrieve his own string representation from a dictionary.
+    // </summary>
+    // <param name="eValue">[IN] The enumeration value.</param>
+    // <param name="nameValueDictionary">[IN] The dictionary where enumeration's string representations are stored.</param>
+    // <returns>
+    // The enumerated value's string representation.
+    // </returns>
+    const string_q& ConvertToString(const EQThreadPriority::EnumType eValue, const TNameValueMap& nameValueDictionary) const
+    {
+        TNameValueMap::const_iterator itValueName = nameValueDictionary.begin();
+        TNameValueMap::const_iterator itValueNameEnd = nameValueDictionary.end();
+
+        while(itValueName != itValueNameEnd && itValueName->second != eValue)
+            ++itValueName;
+
+        if(itValueName != itValueNameEnd)
+            return itValueName->first;
+        else
+        {
+            static const string_q EMPTY_STRING; 
+            return EMPTY_STRING;
+        }
+    }
+
+
+    // ATTRIBUTES
+	// ---------------
+private:
+
+    /// <summary>
+    /// A list of enumeration values with their names.
+    /// </summary>
+    static TNameValuePair sm_arValueName[];
+
+    /// <summary>
+    /// The dictionary which contains each enumeration value by its name.
+    /// </summary>
+    static TNameValueMap  sm_mapValueName;
+
+    /// <summary>
+    /// The contained enumeration value.
+    /// </summary>
+    EQThreadPriority::EnumType m_value;
+
+};
+
+} //namespace Threading
+} //namespace System
+} //namespace QuimeraEngine
+} //namespace Kinesis
+
+#endif // __EQTHREADPRIORITY__
--- source/SQThisThread.cpp
+++ source/SQThisThread.cpp
@@ -86,7 +86,172 @@ string_q SQThisThread::ToString()
     strResult.Append(STRING_PART3);
     return strResult;
 }
-    
+
+#if defined(QE_OS_WINDOWS)
+
+EQThreadPriority SQThisThread::_ConvertFromNativePriority(const int nNativePriority)
+{
+    EQThreadPriority ePriority = EQThreadPriority::E_Normal;
+
+    switch(nNativePriority)
+    {
+    case THREAD_PRIORITY_HIGHEST:
+        ePriority = EQThreadPriority::E_Highest;
+        break;
+    case THREAD_PRIORITY_ABOVE_NORMAL:
+        ePriority = EQThreadPriority::E_High;
+        break;
+    case THREAD_PRIORITY_NORMAL:
+        ePriority = EQThreadPriority::E_Normal;
+        break;
+    case THREAD_PRIORITY_BELOW_NORMAL:
+        ePriority = EQThreadPriority::E_Low;
+        break;
+    case THREAD_PRIORITY_LOWEST:
+        ePriority = EQThreadPriority::E_Lowest;
+        break;
+    }
+
+    return ePriority;
+}
+
+int SQThisThread::_ConvertToNativePriority(const EQThreadPriority &ePriority)
+{
+    int nNativePriority = 0;
+
+    switch(ePriority)
+    {
+    case EQThreadPriority::E_Highest:
+        nNativePriority = THREAD_PRIORITY_HIGHEST;
+        break;
+    case EQThreadPriority::E_High:
+        nNativePriority = THREAD_PRIORITY_ABOVE_NORMAL;
+        break;
+    case EQThreadPriority::E_Normal:
+        nNativePriority = THREAD_PRIORITY_NORMAL;
+        break;
+    case EQThreadPriority::E_Low:
+        nNativePriority = THREAD_PRIORITY_BELOW_NORMAL;
+        break;
+    case EQThreadPriority::E_Lowest:
+        nNativePriority = THREAD_PRIORITY_LOWEST;
+        break;
+    default:
+        break;
+    }
+
+    return nNativePriority;
+}
+
+#elif defined(QE_OS_LINUX)
+
+EQThreadPriority SQThisThread::_ConvertFromNativePriority(const int nNativePriority)
+{
+    EQThreadPriority ePriority = EQThreadPriority::E_Normal;
+
+    switch(nNativePriority)
+    {
+    case THREAD_PRIORITY_HIGHEST:
+        ePriority = EQThreadPriority::E_Highest;
+        break;
+    case THREAD_PRIORITY_ABOVE_NORMAL:
+        ePriority = EQThreadPriority::E_High;
+        break;
+    case THREAD_PRIORITY_NORMAL:
+        ePriority = EQThreadPriority::E_Normal;
+        break;
+    case THREAD_PRIORITY_BELOW_NORMAL:
+        ePriority = EQThreadPriority::E_Low;
+        break;
+    case THREAD_PRIORITY_LOWEST:
+        ePriority = EQThreadPriority::E_Lowest;
+        break;
+    }
+
+    return ePriority;
+}
+
+int SQThisThread::_ConvertToNativePriority(const EQThreadPriority ePriority)
+{
+    int nNativePriority = 0;
+
+    switch(ePriority)
+    {
+    case EQThreadPriority::E_Highest:
+        nNativePriority = THREAD_PRIORITY_HIGHEST;
+        break;
+    case EQThreadPriority::E_High:
+        nNativePriority = THREAD_PRIORITY_ABOVE_NORMAL;
+        break;
+    case EQThreadPriority::E_Normal:
+        nNativePriority = THREAD_PRIORITY_NORMAL;
+        break;
+    case EQThreadPriority::E_Low:
+        nNativePriority = THREAD_PRIORITY_BELOW_NORMAL;
+        break;
+    case EQThreadPriority::E_Lowest:
+        nNativePriority = THREAD_PRIORITY_LOWEST;
+        break;
+    }
+
+    return nNativePriority;
+}
+
+#elif defined(QE_OS_MAC)
+
+EQThreadPriority SQThisThread::_ConvertFromNativePriority(const int nNativePriority)
+{
+    EQThreadPriority ePriority = EQThreadPriority::E_Normal;
+
+    switch(nNativePriority)
+    {
+    case THREAD_PRIORITY_HIGHEST:
+        ePriority = EQThreadPriority::E_Highest;
+        break;
+    case THREAD_PRIORITY_ABOVE_NORMAL:
+        ePriority = EQThreadPriority::E_High;
+        break;
+    case THREAD_PRIORITY_NORMAL:
+        ePriority = EQThreadPriority::E_Normal;
+        break;
+    case THREAD_PRIORITY_BELOW_NORMAL:
+        ePriority = EQThreadPriority::E_Low;
+        break;
+    case THREAD_PRIORITY_LOWEST:
+        ePriority = EQThreadPriority::E_Lowest;
+        break;
+    }
+
+    return ePriority;
+}
+
+int SQThisThread::_ConvertToNativePriority(const EQThreadPriority ePriority)
+{
+    int nNativePriority = 0;
+
+    switch(ePriority)
+    {
+    case EQThreadPriority::E_Highest:
+        nNativePriority = THREAD_PRIORITY_HIGHEST;
+        break;
+    case EQThreadPriority::E_High:
+        nNativePriority = THREAD_PRIORITY_ABOVE_NORMAL;
+        break;
+    case EQThreadPriority::E_Normal:
+        nNativePriority = THREAD_PRIORITY_NORMAL;
+        break;
+    case EQThreadPriority::E_Low:
+        nNativePriority = THREAD_PRIORITY_BELOW_NORMAL;
+        break;
+    case EQThreadPriority::E_Lowest:
+        nNativePriority = THREAD_PRIORITY_LOWEST;
+        break;
+    }
+
+    return nNativePriority;
+}
+
+#endif
 
 //##################=======================================================##################
 //##################             ____________________________              ##################
@@ -123,6 +288,58 @@ QThread::NativeThreadHandle SQThisThread::GetNativeHandle()
 
 #endif
 
+void SQThisThread::SetExitFunction(const Kinesis::QuimeraEngine::Common::QDelegate<void()> function)
+{
+    boost::this_thread::at_thread_exit(function);
+}
+
+#if defined(QE_OS_WINDOWS)
+
+EQThreadPriority SQThisThread::GetPriority()
+{
+    int nNativePriority = ::GetThreadPriority(SQThisThread::GetNativeHandle());
+    return SQThisThread::_ConvertFromNativePriority(nNativePriority);
+}
+
+void SQThisThread::SetPriority(const EQThreadPriority &ePriority)
+{
+    using Kinesis::QuimeraEngine::Common::DataTypes::SQInteger;
+
+    int nNativePriority = SQThisThread::_ConvertToNativePriority(ePriority);
+    BOOL uResult = ::SetThreadPriority(SQThisThread::GetNativeHandle(), nNativePriority);
+    
+    QE_ASSERT_WARNING(uResult != 0, string_q("An unexpected error ocurred when attempting to set the priority of the ") + SQThisThread::ToString() + " to " + ePriority.ToString() + ". The error code is:" + SQInteger::ToString(::GetLastError()) + ".");
+}
+
+#elif defined(QE_OS_LINUX)
+
+EQThreadPriority SQThisThread::GetPriority()
+{
+}
+
+void SQThisThread::SetPriority(const EQThreadPriority &ePriority)
+{
+}
+
+EQThreadPriority SQThisThread::_ConvertFromNativePriority(const int nNativePriority)
+{
+}
+
+#elif defined(QE_OS_MAC)
+
+EQThreadPriority SQThisThread::GetPriority()
+{
+}
+
+void SQThisThread::SetPriority(const EQThreadPriority &ePriority)
+{
+}
+
+EQThreadPriority SQThisThread::_ConvertFromNativePriority(const int nNativePriority)
+{
+}
+
+#endif
 
 } //namespace Threading
 } //namespace System
