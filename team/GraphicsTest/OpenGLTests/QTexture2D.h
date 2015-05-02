1651d87cb8c77e445a1e0c0fdb8feb1394ac7921
lince3d <Lince3D@gmail.com>
2015-05-02 02:49:21 +0000

    Graphics engine prototype: -Now it's possible to enumerate display devices. -Now the screen resolution can be changed. -Now GetFullContent and GetSubtexture methods return the amout of bytes reserved for the output buffer. -Now it's possible to copy a texture to the main framebuffer. -Coordinate correction applied to both textures and renderbuffers when copying to the main framebuffer so origin is Top-Left.

--- team/GraphicsTest/OpenGLTests/QTexture2D.h
+++ team/GraphicsTest/OpenGLTests/QTexture2D.h
@@ -8,6 +8,7 @@
 #include "QSampler2D.h"
 #include "QPixelFormatDescriptor.h"
 #include "ErrorTracingDefinitions.h"
+#include "SQEnumerationToOpenGLConverter.h"
 
 
 class QTexture2D : public QAbstractTexture2D
@@ -35,75 +36,84 @@ public:
         m_uSamples = uMultisamplingSamples;
     }
 
-    virtual void GetSubtexture( const i32_q nXOffset,
-                                const i32_q nYOffset,
-                                const u32_q uWidth,
-                                const u32_q uHeight,
-                                const u8_q uMipmapLevel,
-                                const u32_q uSubtextureSize,
-                                void** ppOutputSubtexture) const
+   virtual void GetSubtexture(  const i32_q nSourceOffsetX,
+                                const i32_q nSourceOffsetY,
+                                const u32_q nSourceWidth,
+                                const u32_q nSourceHeight,
+                                const EQTextureFormat::EnumType eSourceFormat,
+                                const u8_q uSourceMipmapLevel,
+                                u32_q &uSourceSize,
+                                void** ppDestination) const
     {
-        Y AXIS OF TEXTURE MUST BE FLIPPED
+//        Y AXIS OF TEXTURE MUST BE FLIPPED
 
-        QE_ASSERT_ERROR(nXOffset + uWidth > m_uWidth, "The region to be read lays outside the texture's dimensions.");
-        QE_ASSERT_ERROR(nYOffset + uHeight > m_uHeight, "The region to be read lays outside the texture's dimensions.");
+        QE_ASSERT_ERROR(nSourceOffsetX + nSourceWidth <= m_uWidth, "The region to be read lays outside the texture's dimensions.");
+        QE_ASSERT_ERROR(nSourceOffsetY + nSourceHeight <= m_uHeight, "The region to be read lays outside the texture's dimensions.");
         QE_ASSERT_ERROR(m_uSamples == 0, "Reading a region of a multisample texture is not supported.");
-        QE_ASSERT_ERROR(ppOutputSubtexture != null_q, "The output subtexture cannot be null.");
+        QE_ASSERT_ERROR(ppDestination != null_q, "The output subtexture cannot be null.");
 
-        u32_q uFinalSubtextureSize = uSubtextureSize;
-
-        if (*ppOutputSubtexture == null_q)
+        if (*ppDestination == null_q)
         {
-            QPixelFormatDescriptor pixelFormatDescriptor(scast_q(m_eFormat, EQPixelFormat::EnumType));
+            QPixelFormatDescriptor pixelFormatDescriptor(scast_q(eSourceFormat, EQPixelFormat::EnumType));
 
             // Allocate memory
-            uFinalSubtextureSize = pixelFormatDescriptor.GetSize() * uWidth * uHeight;
-            *ppOutputSubtexture = new u8_q[uFinalSubtextureSize];
+            uSourceSize = pixelFormatDescriptor.GetSize() * nSourceWidth * nSourceHeight;
+            *ppDestination = new u8_q[uSourceSize];
         }
+
+        GLenum formatType = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataType(eSourceFormat);
+        GLenum formatLayout = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataFormatNonSized(eSourceFormat);
 
         static const GLint FIRST_SLICE = 0;
         static const GLsizei NUM_SLICES = 1;
-        glGetTextureSubImage(m_textureId, uMipmapLevel, nXOffset, nYOffset, FIRST_SLICE, uWidth, uHeight, NUM_SLICES, m_format, m_type, uFinalSubtextureSize, *ppOutputSubtexture);
+        glGetTextureSubImage(m_textureId, uSourceMipmapLevel, nSourceOffsetX, nSourceOffsetY, FIRST_SLICE, nSourceWidth, nSourceHeight, NUM_SLICES, formatLayout, formatType, uSourceSize, *ppDestination);
 
         QE_ASSERT_OPENGL_ERROR("An error occurred when reading from the texture.");
     }
 
-    virtual void GetFullContent(const u8_q uMipmapLevel, const u32_q uSubtextureSize, void** ppOutputSubtexture) const
+    virtual void GetFullContent(const EQTextureFormat::EnumType eSourceFormat, const u8_q uDestinationMipmapLevel, u32_q &uDestinationSize, void** ppDestination) const
     {
-        QE_ASSERT_ERROR(ppOutputSubtexture != null_q, "The output subtexture cannot be null.");
+        QE_ASSERT_ERROR(ppDestination != null_q, "The output subtexture cannot be null.");
 
-        u32_q uFinalSubtextureSize = uSubtextureSize;
-
-        if (*ppOutputSubtexture == null_q)
+        if (*ppDestination == null_q)
         {
-            QPixelFormatDescriptor pixelFormatDescriptor(scast_q(m_eFormat, EQPixelFormat::EnumType));
+            QPixelFormatDescriptor pixelFormatDescriptor(scast_q(eSourceFormat, EQPixelFormat::EnumType));
 
             // Allocate memory
-            uFinalSubtextureSize = pixelFormatDescriptor.GetSize() * m_uWidth * m_uHeight;
-            uFinalSubtextureSize *= m_uSamples > 0 ? m_uSamples : 1U;
-            *ppOutputSubtexture = new u8_q[uFinalSubtextureSize];
+            uDestinationSize = pixelFormatDescriptor.GetSize() * m_uWidth * m_uHeight;
+            uDestinationSize *= m_uSamples > 0 ? m_uSamples : 1U;
+            *ppDestination = new u8_q[uDestinationSize];
         }
 
-        glGetTextureImage(m_textureId, uMipmapLevel, m_format, m_type, uFinalSubtextureSize, *ppOutputSubtexture);
+        GLenum formatType = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataType(eSourceFormat);
+        GLenum formatLayout = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataFormatNonSized(eSourceFormat);
+
+        glGetTextureImage(m_textureId, uDestinationMipmapLevel, formatLayout, formatType, uDestinationSize, *ppDestination);
 
         QE_ASSERT_OPENGL_ERROR("An error occurred when reading from the texture.");
     }
 
     // COPY full and region
 
-    virtual void SetFullContent(const u8_q uMipmapLevel, const void* pSubtexture)
+    virtual void SetFullContent(const u8_q uDestinationMipmapLevel, const EQTextureFormat::EnumType eSourceFormat, const void* pSource)
     {
-        glTextureSubImage2D(m_textureId, uMipmapLevel, 0, 0, m_uWidth, m_uHeight, m_format, m_type, pSubtexture);
+        GLenum formatType = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataType(eSourceFormat);
+        GLenum formatLayout = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataFormatNonSized(eSourceFormat);
+
+        glTextureSubImage2D(m_textureId, uDestinationMipmapLevel, 0, 0, m_uWidth, m_uHeight, formatLayout, formatType, pSource);
 
         QE_ASSERT_OPENGL_ERROR("An error occurred when writing to the texture.");
     }
-    // [TODO]: Rename to nDestinationOffsetX , etc..
-    virtual void SetSubtexture(const i32_q nXOffset, const i32_q nYOffset, const u32_q uWidth, const u32_q uHeight, const u8_q uMipmapLevel, const void* pSubtexture)
-    {
-        QE_ASSERT_ERROR(nXOffset + uWidth > m_uWidth, "The region to be written overwrites memory outside the texture's dimensions.");
-        QE_ASSERT_ERROR(nYOffset + uHeight > m_uHeight, "The region to be written overwrites memory outside the texture's dimensions.");
 
-        glTextureSubImage2D(m_textureId, uMipmapLevel, nXOffset, nYOffset, uWidth, uHeight, m_format, m_type, pSubtexture);
+    virtual void SetSubtexture(const i32_q nDestinationOffsetX, const i32_q nDestinationOffsetY, const u32_q nDestinationWidth, const u32_q nDestinationHeight, const u8_q uDestinationMipmapLevel, const EQTextureFormat::EnumType eSourceFormat, const void* pSource)
+    {
+        QE_ASSERT_ERROR(nDestinationOffsetX + nDestinationWidth <= m_uWidth, "The region to be written overwrites memory outside the texture's dimensions.");
+        QE_ASSERT_ERROR(nDestinationOffsetY + nDestinationHeight <= m_uHeight, "The region to be written overwrites memory outside the texture's dimensions.");
+
+        GLenum formatType = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataType(eSourceFormat);
+        GLenum formatLayout = SQEnumerationToOpenGLConverter::ConvertPixelFormatToPixelDataFormatNonSized(eSourceFormat);
+
+        glTextureSubImage2D(m_textureId, uDestinationMipmapLevel, nDestinationOffsetX, nDestinationOffsetY, nDestinationWidth, nDestinationHeight, formatLayout, formatType, pSource);
 
         QE_ASSERT_OPENGL_ERROR("An error occurred when writing to the texture.");
     }
--- team/GraphicsTest/OpenGLTests/SQSystemManager.h
+++ team/GraphicsTest/OpenGLTests/SQSystemManager.h
@@ -4,24 +4,44 @@
 #include "QuimeraEngineIncludesAndUsings.h"
 #include "QWindow.h"
 
+// [TODO]: Split this class among every component (peripherals, audio, graphics, etc.)
 class SQSystemManager // [TODO]: Look for a better name
 {
 public:
 
-    struct QScreenInfo
+    struct QScreenResolution
     {
         u32_q Width;
         u32_q Height;
     };
 
-
-    static void ChangeScreenResolution()
+    struct QGraphicsDeviceInfo
     {
+        string_q GraphicsDeviceId;
+        string_q GraphicsDeviceName;
+        bool IsPrimary;
+    };
+    
+    static void ChangeScreenResolution(const QScreenResolution resolution, const u32_q uBitDepth)
+    {
+        DEVMODE settings;
+        settings.dmSize = sizeof(DEVMODE);
+        ::EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &settings);
+
+        if (settings.dmPelsHeight != resolution.Height ||
+            settings.dmPelsWidth != resolution.Width   ||
+            settings.dmBitsPerPel != uBitDepth)
+        {
+            settings.dmPelsHeight = resolution.Height;
+            settings.dmPelsWidth = resolution.Width;
+            settings.dmBitsPerPel = uBitDepth;
+            ::ChangeDisplaySettings(&settings, 0);
+        }
     }
 
-    static QScreenInfo GetWindowScreen(QWindow* pWindow)
+    static QScreenResolution GetWindowScreen(QWindow* pWindow)
     {
-        QScreenInfo info;
+        QScreenResolution info;
 
         MONITORINFO monitorInfo;
         monitorInfo.cbSize = sizeof(MONITORINFO);
@@ -33,12 +53,71 @@ public:
         return info;
     }
 
-    static void EnumerateScreens()
+    static BOOL CALLBACK _EnumerateDisplayMonitorsCallback( _In_ HMONITOR hMonitor,
+                                                            _In_ HDC hdcMonitor,
+                                                            _In_ LPRECT lprcMonitor,
+                                                            _In_ LPARAM dwData)
     {
+        QE_LOG(string_q("- ") + u32_q(lprcMonitor->right - lprcMonitor->left) + "x" + u32_q(lprcMonitor->bottom - lprcMonitor->top) + "\n");
+        return TRUE;
     }
 
-    static void EnumerateGraphicDevices()
+    static void EnumerateScreens()
     {
+        QE_LOG("Listing monitors:\n");
+        ::EnumDisplayMonitors(NULL, NULL, &SQSystemManager::_EnumerateDisplayMonitorsCallback, 0);
+    }
+
+    static void EnumerateGraphicsDevices()
+    {
+        QE_LOG("Listing display devices:\n");
+
+        PDISPLAY_DEVICE pDisplayDevice = new DISPLAY_DEVICE;
+        pDisplayDevice->cb = sizeof(DISPLAY_DEVICE);
+
+        QArrayDynamic<DISPLAY_DEVICE> arDevices(5);
+
+        int i = 0;
+
+        while (::EnumDisplayDevices(NULL, i, pDisplayDevice, EDD_GET_DEVICE_INTERFACE_NAME))
+        {
+            if (pDisplayDevice->StateFlags & DISPLAY_DEVICE_ACTIVE)
+            {
+                QE_LOG(string_q("- ") + string_q(pDisplayDevice->DeviceName) + "\n");
+                QE_LOG(string_q("  ") + string_q(pDisplayDevice->DeviceString) + "\n");
+                QE_LOG(string_q("  ") + string_q(pDisplayDevice->DeviceID) + "\n");
+                QE_LOG(string_q("  ") + string_q(pDisplayDevice->DeviceKey) + "\n");
+                arDevices.Add(*pDisplayDevice);
+            }
+            ++i;
+        }
+
+        delete pDisplayDevice;
+
+        QE_LOG("\n");
+        QE_LOG("Listing display settings:\n");
+
+        i = 0;
+        DEVMODE* pDisplaySettings = new DEVMODE;
+        pDisplaySettings->dmSize = sizeof(DEVMODE);
+
+        for (pointer_uint_q iDisplayDevice = 0; iDisplayDevice < arDevices.GetCount(); ++iDisplayDevice)
+        {
+            QE_LOG(string_q("Device: ") + string_q(arDevices[iDisplayDevice].DeviceName) + "\n");
+
+            while (::EnumDisplaySettings(arDevices[iDisplayDevice].DeviceName, i, pDisplaySettings))
+            {
+                if (pDisplaySettings->dmBitsPerPel >= 32)
+                {
+                    QE_LOG(string_q("- ") + (u32_q)pDisplaySettings->dmPelsWidth + "x" + (u32_q)pDisplaySettings->dmPelsHeight + " - " + (u32_q)pDisplaySettings->dmBitsPerPel + " bits\n");
+                }
+                ++i;
+            }
+        }
+
+        delete pDisplaySettings;
+
+        // [TODO]: Remove repeated elements
     }
 
     static void EnumerateAudioDevices()
